403e761aff8c23941dcdc1f348cd1d6dddb490cc
Kanishka Azimi <kazimi@solvuu.com>
2020-12-16 14:54:21 -0500

    support page: content copied and styled without links

--- site/res_pages/community/CommunitySupport.js
+++ site/res_pages/community/CommunitySupport.js
@@ -2,6 +2,13 @@
 
 import * as React from "react";
 
+function CommunitySupport$P(Props) {
+  var children = Props.children;
+  return React.createElement("p", {
+              className: "mb-6"
+            }, children);
+}
+
 function CommunitySupport$H1(Props) {
   var children = Props.children;
   return React.createElement("h1", {
@@ -26,20 +33,34 @@ function CommunitySupport$H3(Props) {
 function $$default(param) {
   return React.createElement(React.Fragment, undefined, React.createElement(CommunitySupport$H1, {
                   children: "Support"
+                }), React.createElement(CommunitySupport$P, {
+                  children: "A great way to get free support is by using\n     the active mailing lists. When you need to go beyond this \n     and get professional support, you have the following \n     options:"
                 }), React.createElement(CommunitySupport$H2, {
                   children: "Commercial Support"
                 }), React.createElement(CommunitySupport$H3, {
                   children: "The OCamlPro Company"
+                }), React.createElement(CommunitySupport$P, {
+                  children: "OCamlPro is the creator of many open-source \n    tools widely used throughout the community, such as Try OCaml, \n    the OPAM package manager and ocp-indent, as well as a large \n    contributor to OCaml itself. Besides commercially supporting \n    their tools, they offer to share their expertise through full \n    OCaml support packages. They also provide trainings and \n    specialized software developments."
+                }), React.createElement(CommunitySupport$P, {
+                  children: "OCamlPro is an INRIA spin-off with a team \n    of highly skilled experienced OCaml programmers, including \n    members of the OCaml core development team, and they have \n    expertise to help debug and optimize OCaml projects as \n    well as improve specific work environments. See details \n    here."
                 }), React.createElement(CommunitySupport$H3, {
                   children: "Gerd Stolpmann"
+                }), React.createElement(CommunitySupport$P, {
+                  children: "Gerd Stolpmann has been helping companies master \n    OCaml since 2005. He is an expert of the ecosystem surrounding \n    OCaml and developed the GODI platform. Stolpmann is a computer \n    scientist who has been a contractor for several long-running \n    OCaml projects. He has a focus on big data (including data \n    preparation, search/query engines, map/reduce), but his skills \n    also cover Unix system programming, SQL databases, \n    client/server, compiler development (e.g. for domain-specific languages), \n    and much more. Also visit his website on OCaml."
                 }), React.createElement(CommunitySupport$H2, {
                   children: "Giving Support"
                 }), React.createElement(CommunitySupport$H3, {
                   children: "Caml Consortium at Inria"
+                }), React.createElement(CommunitySupport$P, {
+                  children: "You can join the Caml Consortium to support \n    development of the OCaml compiler itself. See details \n    here."
                 }), React.createElement(CommunitySupport$H3, {
                   children: "OCaml Labs"
+                }), React.createElement(CommunitySupport$P, {
+                  children: "You can support OCaml Labs, which runs a \n    variety of open source projects to support the OCaml community. \n    See details here."
                 }), React.createElement(CommunitySupport$H3, {
                   children: "IRILL"
+                }), React.createElement(CommunitySupport$P, {
+                  children: "You can support IRILL, the Center for Research \n    and Innovation on Free Software, a major actor of the OCaml \n    community, with projects such as js_of_ocaml (the OCaml \n    to JavaScript optimizing compiler), Dose (a key component of \n    the OPAM package manager). They also use OCaml to contribute to \n    other major open source projects (Coccinelle, for example). \n    See details here."
                 }));
 }
 
--- site/res_pages/community/CommunitySupport.res
+++ site/res_pages/community/CommunitySupport.res
@@ -1,5 +1,9 @@
 module Link = Next.Link;
 
+module P = {
+  @react.component
+  let make = (~children) => <p className="mb-6"> children </p>;
+};
 module H1 = {
   // TODO: use mb-1.5 and tailwind 2
   @react.component
@@ -17,16 +21,6 @@ module H3 = {
   let make = (~children) => 
     <h3 className="font-sans text-lg font-bold leading-normal mb-3"> children </h3>;
 };
-module UL = {
-  @react.component
-  let make = (~children) => 
-    <ul className="mb-6 ml-6 -mt-3 list-disc"> children </ul>;
-};
-module LI = {
-  @react.component
-  let make = (~children) => 
-    <li className="mb-3"> children </li>;
-};
 // TODO: I haven't been able to implement "A" without include "Link" yet. Troubleshoot why
 module LINK = {
   @react.component
@@ -42,18 +36,61 @@ module AEXT = {
 let default = () =>
   <>
     <H1>{React.string(`Support`)}</H1>
+    <P>{React.string(`A great way to get free support is by using
+     the active mailing lists. When you need to go beyond this 
+     and get professional support, you have the following 
+     options:`)}
+    </P>
 
     <H2>{React.string(`Commercial Support`)}</H2>
 
     <H3>{React.string(`The OCamlPro Company`)}</H3>
+    <P>{React.string(`OCamlPro is the creator of many open-source 
+    tools widely used throughout the community, such as Try OCaml, 
+    the OPAM package manager and ocp-indent, as well as a large 
+    contributor to OCaml itself. Besides commercially supporting 
+    their tools, they offer to share their expertise through full 
+    OCaml support packages. They also provide trainings and 
+    specialized software developments.`)}
+    </P>
+    <P>{React.string(`OCamlPro is an INRIA spin-off with a team 
+    of highly skilled experienced OCaml programmers, including 
+    members of the OCaml core development team, and they have 
+    expertise to help debug and optimize OCaml projects as 
+    well as improve specific work environments. See details 
+    here.`)}
+    </P>
+
 
     <H3>{React.string(`Gerd Stolpmann`)}</H3>
+    <P>{React.string(`Gerd Stolpmann has been helping companies master 
+    OCaml since 2005. He is an expert of the ecosystem surrounding 
+    OCaml and developed the GODI platform. Stolpmann is a computer 
+    scientist who has been a contractor for several long-running 
+    OCaml projects. He has a focus on big data (including data 
+    preparation, search/query engines, map/reduce), but his skills 
+    also cover Unix system programming, SQL databases, 
+    client/server, compiler development (e.g. for domain-specific languages), 
+    and much more. Also visit his website on OCaml.`)}</P>
 
     <H2>{React.string(`Giving Support`)}</H2>
-
+    
     <H3>{React.string(`Caml Consortium at Inria`)}</H3>
+    <P>{React.string(`You can join the Caml Consortium to support 
+    development of the OCaml compiler itself. See details 
+    here.`)}</P>
 
     <H3>{React.string(`OCaml Labs`)}</H3>
+    <P>{React.string(`You can support OCaml Labs, which runs a 
+    variety of open source projects to support the OCaml community. 
+    See details here.`)}</P>
 
     <H3>{React.string(`IRILL`)}</H3>
+    <P>{React.string(`You can support IRILL, the Center for Research 
+    and Innovation on Free Software, a major actor of the OCaml 
+    community, with projects such as js_of_ocaml (the OCaml 
+    to JavaScript optimizing compiler), Dose (a key component of 
+    the OPAM package manager). They also use OCaml to contribute to 
+    other major open source projects (Coccinelle, for example). 
+    See details here.`)}</P>
   </>;
