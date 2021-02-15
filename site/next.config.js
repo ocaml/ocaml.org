6da3dde9470f0c429c04020e57f7cff724db9426
kanishka-work <1385802+kanishka-work@users.noreply.github.com>
2021-02-15 16:13:00 -0500

    Around the web: update call to action, links
    - update call to action to use updated tailwind UI component selection
    - update links to be more reponsive

--- site/next.config.js
+++ site/next.config.js
@@ -36,7 +36,7 @@ const config = {
 
       {
         source: '/play/community/aroundweb',
-        destination: 'https://play.tailwindcss.com/BxXxU2gs9A?layout=preview',
+        destination: 'https://play.tailwindcss.com/jUYpyl33nV?layout=preview',
         permanent: false,
       },
       {
