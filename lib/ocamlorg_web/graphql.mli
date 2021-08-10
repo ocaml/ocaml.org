af1fa8f09b5c9cae06164f64f7cdb24bde48e3f7
Odinaka Joy <odinakajoy909@gmail.com>
2021-08-10 05:50:22 +0100

    rebased on pull 73 to pass test

--- lib/ocamlorg_web/graphql.mli
+++ lib/ocamlorg_web/graphql.mli
@@ -30,6 +30,6 @@ val get_info : (Package.Name.t * string option) list -> package_info list
 (** This function returns the list part of Package.Info such as
     Package.Info.dependencies *)
 
-val schema: t:Package.state -> Dream.request Graphql_lwt.Schema.schema
+val schema : t:Package.state -> Dream.request Graphql_lwt.Schema.schema
 (** This schema allows to query for opam packages from the opam-repository as
     graphql api data *)
