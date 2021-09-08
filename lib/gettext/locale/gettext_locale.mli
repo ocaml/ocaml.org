e98d8d8825822dc1014447645eec02f7a718702b
Thibaut <thibaut.mattio@gmail.com>
2021-09-08 22:17:54 +0200

    Add gettext library

---
+++ lib/gettext/locale/gettext_locale.mli
@@ -0,0 +1,12 @@
+type t =
+  { language : string
+  ; territory : string option
+  ; codeset : string option
+  ; modifier : string option
+  }
+
+val of_string : string -> t
+
+val to_string : t -> string
+
+val compare : t -> t -> int
