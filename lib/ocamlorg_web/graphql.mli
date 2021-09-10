dd32e82422603eee368cc86d9b5e472390cffa84
Odinaka Joy <odinakajoy909@gmail.com>
2021-09-10 10:25:55 +0100

    Improves the packages GraphQL API (#78)

--- lib/ocamlorg_web/graphql.mli
+++ lib/ocamlorg_web/graphql.mli
@@ -5,41 +5,91 @@ type package_info =
   ; constraints : string option
   }
 
-type packages_result =
+type packages_success =
   { total_packages : int
   ; packages : Package.t list
   }
 
-val starts_with : string -> string -> bool
-(** [starts_with] function compares two strings and returns true if the first
-    string is equal to the beginning of the second string up to capitalization *)
+type packages_result = (packages_success, [ `Msg of string ]) result
 
-val is_package : string -> string -> bool
-(** [is_package] function compares two strings and returns true if they are
-    equal up to capitalization *)
+type package_success = { package : Package.t }
 
-val get_packages_result
-  :  int
-  -> int
-  -> int
-  -> string option
-  -> Package.t list
-  -> packages_result
-(** [get_packages_result total_packages offset limit startswith packages]
-    returns a list of the latest version of all packages with [limit] and
-    [offset] options which is used to cut out some parts of the
-    [packages_result] list. It also takes a [startswith] option which will
-    search the list of packages looking for the package with a name that begins
-    with [startswith] *)
+type package_result = (package_success, [ `Msg of string ]) result
 
-val get_single_package : Package.t list -> string -> Package.t option
-(** [get_single_package packages name] will search the list of packages looking
-    for the package with a name that matches [name], [None] is returned if no
-    package can be found. *)
+type params_validity =
+  | Valid_params
+  | Wrong_limit
+  | Wrong_offset
 
 val get_info : (Package.Name.t * string option) list -> package_info list
 (** [get_info] function loops through the list part of Package.Info such as
-    Package.Info.dependencies and returns a list of name and constraints record *)
+    Package.Info.dependencies and returns a list of name and constraints from
+    the Package.Info record *)
+
+val is_in_range
+  :  Package.Version.t
+  -> Package.Version.t option
+  -> Package.Version.t option
+  -> bool
+(** [is_in_range] function takes a [current_version] and an optional
+    [from_version] and [upto_version], then checks that current_version is
+    between the range of from_version and upto_version *)
+
+val is_valid_params : int -> int -> int -> params_validity
+(** [is_valid_params] function is a sub function of [all_packages_result] that
+    takes [offset] [limit] [all_packages] and retruns an error message of type
+    [params_validity]if limit or ossfest is less than 0 or gretaer than
+    [all_packages] *)
+
+val packages_list
+  :  ?contains:string
+  -> int
+  -> int
+  -> Package.t list
+  -> Package.state
+  -> Package.t list
+(** [packages_list] function is a sub function of [all_packages_result] that
+    takes an optional [contains] together with [offset], [limit], [all_packages]
+    and [state] parameters. It applies the [offset] and [limit] specified on
+    [all_packages] if no value is specified for [contains] else it calls
+    [Package.search_package] passing the [contains] value to get a list of
+    packages that has [contains] in its name *)
+
+val all_packages_result
+  :  ?contains:string
+  -> int
+  -> int option
+  -> Package.state
+  -> (packages_success, string) result
+(** [packages_list] function takes [contains] [offset] [limit] [all_packages]
+    [state] and returns a list of the latest version of all packages with
+    [limit] and [offset] options which is used to cut out some parts of the
+    [packages_result] list. It also takes a [contains] option which will search
+    the list of packages looking for the package with a name that has [contains]
+    in it *)
+
+val package_result
+  :  string
+  -> string option
+  -> Package.state
+  -> (Package.t, string) result
+(** [package_result] function takes [name] [version] [state] and returns the
+    latest version of the package with a name that matches [name] if no version
+    is specified else it returns the package with name and version that match
+    with [name] and [version]. An error message is returned if no package was
+    found. *)
+
+val package_versions_result
+  :  string
+  -> string option
+  -> string option
+  -> Package.state
+  -> (packages_success, string) result
+(** [package_versions_result] function takes [name] [from] [upto] [state] and
+    returns the all versions of the package with a name that matches [name] if
+    no [from] or [upto] version is specified else it returns the package [from]
+    to [upto] versions specified. An error message is returned if no package or
+    versions was found. *)
 
 val schema : Package.state -> Dream.request Graphql_lwt.Schema.schema
 (** This schema allows to query for opam packages from the opam-repository as
--- test/ocamlorg_web/graphql_test.ml
+++ test/ocamlorg_web/graphql_test.ml
@@ -1,3 +1,4 @@
+module Ocamlorg_web = Ocamlorg_web
 module Package = Ocamlorg.Package
 
 let dependencies =
@@ -20,7 +21,7 @@ let packages : Package.t list =
       ; license = "ISC"
       ; homepage = []
       ; tags = []
-      ; dependencies = []
+      ; dependencies
       ; depopts = []
       ; conflicts = []
       ; url = None
@@ -35,7 +36,7 @@ let packages : Package.t list =
       ; license = "ISC"
       ; homepage = []
       ; tags = []
-      ; dependencies = []
+      ; dependencies
       ; depopts = []
       ; conflicts = []
       ; url = None
@@ -50,7 +51,7 @@ let packages : Package.t list =
       ; license = "ISC"
       ; homepage = []
       ; tags = []
-      ; dependencies = []
+      ; dependencies
       ; depopts = []
       ; conflicts = []
       ; url = None
@@ -71,8 +72,40 @@ let packages : Package.t list =
       ; url = None
       }
   ; Package.create
-      ~name:(Package.Name.of_string "absolute")
+      ~name:(Package.Name.of_string "merlin")
       ~version:(Package.Version.of_string "2.1.0")
+      { Package.Info.synopsis =
+          "Interactive theorem prover based on lambda-tree syntax"
+      ; description = ""
+      ; authors = []
+      ; maintainers = []
+      ; license = "ISC"
+      ; homepage = []
+      ; tags = []
+      ; dependencies = []
+      ; depopts = []
+      ; conflicts = []
+      ; url = None
+      }
+  ; Package.create
+      ~name:(Package.Name.of_string "merlin")
+      ~version:(Package.Version.of_string "3.5.0")
+      { Package.Info.synopsis =
+          "Interactive theorem prover based on lambda-tree syntax"
+      ; description = ""
+      ; authors = []
+      ; maintainers = []
+      ; license = "ISC"
+      ; homepage = []
+      ; tags = []
+      ; dependencies = []
+      ; depopts = []
+      ; conflicts = []
+      ; url = None
+      }
+  ; Package.create
+      ~name:(Package.Name.of_string "merlin")
+      ~version:(Package.Version.of_string "1.1.0")
       { Package.Info.synopsis =
           "Interactive theorem prover based on lambda-tree syntax"
       ; description = ""
@@ -88,49 +121,89 @@ let packages : Package.t list =
       }
   ]
 
-let start_with_test str1 str2 cond () =
-  Alcotest.(check bool)
-    "is true or false"
-    cond
-    (Ocamlorg_web.Graphql.starts_with str1 str2)
-
-let is_package_test name1 name2 cond () =
-  Alcotest.(check bool)
-    "is false"
-    cond
-    (Ocamlorg_web.Graphql.is_package name1 name2)
-
-let get_packages_result_test () =
-  let total_packages = List.length packages in
-  let offset = 0 in
-  let limit = 3 in
-  let startswith = None in
-  let all_packages =
-    Ocamlorg_web.Graphql.get_packages_result
-      total_packages
-      offset
-      limit
-      startswith
-      packages
-  in
-  let num_of_pakgs = List.length all_packages.packages in
-  Alcotest.(check int) "returns 3 packages" limit num_of_pakgs
-
 let get_info_test () =
   let deps = Ocamlorg_web.Graphql.get_info dependencies in
-  let num_of_deps = List.length deps in
-  Alcotest.(check int) "returns 6 dependencies" 6 num_of_deps
+  let num_of_deps_returned = List.length deps in
+  Alcotest.(check int)
+    "returns 6 dependencies"
+    (List.length dependencies)
+    num_of_deps_returned
 
-let get_single_package_test name () =
-  let pkg = Ocamlorg_web.Graphql.get_single_package packages name in
-  match pkg with
-  | None ->
-    Alcotest.fail "Expected a package"
-  | Some package ->
-    Alcotest.(check string)
-      "Same name"
-      name
-      (Package.Name.to_string (Package.name package))
+let is_in_range_test current_version from_version upto_version cond () =
+  let is_in_range =
+    Ocamlorg_web.Graphql.is_in_range current_version from_version upto_version
+  in
+  Alcotest.(check bool) "returns true or false" cond is_in_range
+
+let is_valid_params_test limit offset cond () =
+  let total_packages = List.length packages in
+  let is_valid_params =
+    Ocamlorg_web.Graphql.is_valid_params limit offset total_packages
+  in
+  let is_valid_params =
+    match is_valid_params with
+    | Wrong_limit ->
+      "wrong_limit"
+    | Wrong_offset ->
+      "wrong_offset"
+    | _ ->
+      "valid_params"
+  in
+  Alcotest.(check string) ("returns " ^ cond) cond is_valid_params
+
+let packages_list_test ?contains offset limit total_length () =
+  let state = Package.state_of_package_list packages in
+  let all_packages =
+    Ocamlorg_web.Graphql.packages_list ?contains offset limit packages state
+  in
+  let num_of_packages_returned = List.length all_packages in
+  Alcotest.(check int)
+    "returns all matched packages"
+    total_length
+    num_of_packages_returned
+
+let all_packages_result_test ?contains offset limit () =
+  let state = Package.state_of_package_list packages in
+  let all_packages =
+    Ocamlorg_web.Graphql.all_packages_result ?contains offset limit state
+  in
+  let num_of_packages_returned =
+    match all_packages with
+    | Error _ ->
+      0
+    | Ok all_packages ->
+      List.length all_packages.packages
+  in
+  Alcotest.(check int) "returns all the packages" 5 num_of_packages_returned
+
+let package_result_test name version expect () =
+  let state = Package.state_of_package_list packages in
+  let package = Ocamlorg_web.Graphql.package_result name version state in
+  let result =
+    match package with
+    | Error _ ->
+      "Not Found"
+    | Ok package ->
+      package |> Package.name |> Package.Name.to_string
+  in
+  Alcotest.(check string) "same package" expect result
+
+let package_versions_result_test name from upto total_packages () =
+  let state = Package.state_of_package_list packages in
+  let package_versions =
+    Ocamlorg_web.Graphql.package_versions_result name from upto state
+  in
+  let num_of_packages_returned =
+    match package_versions with
+    | Error _ ->
+      0
+    | Ok package_versions ->
+      List.length package_versions.packages
+  in
+  Alcotest.(check int)
+    "returns all package versions"
+    total_packages
+    num_of_packages_returned
 
 let state_test () =
   let state = Package.state_of_package_list packages in
@@ -145,48 +218,97 @@ let state_test () =
 let () =
   Alcotest.run
     "ocamlorg"
-    [ ( "test that starts_with function compares two similar strings"
+    [ ( "get_info_test"
       , [ Alcotest.test_case
-            "and returns true"
-            `Quick
-            (start_with_test "OC" "oc" true)
-        ] )
-    ; ( "test that starts_with function compares two different strings"
-      , [ Alcotest.test_case
-            "and returns false"
-            `Quick
-            (start_with_test "gv" "Bt" false)
-        ] )
-    ; ( "test that is_package function compares the same names"
-      , [ Alcotest.test_case
-            "and returns true"
-            `Quick
-            (is_package_test "ocaml" "ocaml" true)
-        ] )
-    ; ( "test that is_package function compares two different names"
-      , [ Alcotest.test_case
-            "and returns false"
-            `Quick
-            (is_package_test "ocaml" "merlin" false)
-        ] )
-    ; ( "test get_packages_result function with limit to 3 option"
-      , [ Alcotest.test_case
-            "and it returns 3 packages"
-            `Quick
-            get_packages_result_test
-        ] )
-    ; ( "test that get_info function works"
-      , [ Alcotest.test_case
-            "and returns all 6 dependencies"
+            "returns the number of dependencies found"
             `Quick
             get_info_test
         ] )
-    ; ( "test to get a single package by name that exist"
+    ; ( "is_in_range_test_within_range"
       , [ Alcotest.test_case
             "returns true"
             `Quick
-            (get_single_package_test "ocaml")
+            (is_in_range_test
+               (Package.Version.of_string "3.12.0")
+               (Some (Package.Version.of_string "2.0.0"))
+               (Some (Package.Version.of_string "5.9.2"))
+               true)
         ] )
-    ; ( "state test"
-      , [ Alcotest.test_case "same package from state" `Quick state_test ] )
+    ; ( "is_in_range_test_outside_range"
+      , [ Alcotest.test_case
+            "returns false"
+            `Quick
+            (is_in_range_test
+               (Package.Version.of_string "3.12.0")
+               None
+               (Some (Package.Version.of_string "2.97.2"))
+               false)
+        ] )
+    ; ( "is_valid_params_test_valid_params"
+      , [ Alcotest.test_case
+            "returns valid_params"
+            `Quick
+            (is_valid_params_test (List.length packages) 0 "valid_params")
+        ] )
+    ; ( "is_valid_params_test_wrong_limit"
+      , [ Alcotest.test_case
+            "returns wrong_limit"
+            `Quick
+            (is_valid_params_test 0 0 "wrong_limit")
+        ] )
+    ; ( "is_valid_params_test_wrong_offset"
+      , [ Alcotest.test_case
+            "returns wrong_offset"
+            `Quick
+            (is_valid_params_test (List.length packages) 200 "wrong_offset")
+        ] )
+    ; ( "packages_list_test_no_contains"
+      , [ Alcotest.test_case
+            "returns all packages"
+            `Quick
+            (packages_list_test 0 (List.length packages) (List.length packages))
+        ] )
+    ; ( "packages_list_test_with_contains"
+      , [ Alcotest.test_case
+            "returns the number of packages that has 'abt' in its name"
+            `Quick
+            (packages_list_test ~contains:"abt" 0 (List.length packages) 1)
+        ] )
+    ; ( "all_packages_result_test"
+      , [ Alcotest.test_case
+            "returns the number of packages found"
+            `Quick
+            (all_packages_result_test 0 None)
+        ] )
+    ; ( "package_result_test_with_valid_name"
+      , [ Alcotest.test_case
+            "returns package name"
+            `Quick
+            (package_result_test "ocaml" None "ocaml")
+        ] )
+    ; ( "package_result_test_with_invalid_name"
+      , [ Alcotest.test_case
+            "returns error message"
+            `Quick
+            (package_result_test "ocdfggaml" None "Not Found")
+        ] )
+    ; ( "package_result_test_with_valid_name_and_version"
+      , [ Alcotest.test_case
+            "returns the package name"
+            `Quick
+            (package_result_test "0install-gtk" (Some "2.1.0") "0install-gtk")
+        ] )
+    ; ( "package_versions_result_test"
+      , [ Alcotest.test_case
+            "returns number of package versions found"
+            `Quick
+            (package_versions_result_test
+               "merlin"
+               (Some "1.0.0")
+               (Some "4.1.0")
+               3)
+        ] )
+    ; ( "state_test"
+      , [ Alcotest.test_case "returns same package from state" `Quick state_test
+        ] )
     ]
