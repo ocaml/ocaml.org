d74d2f4f5bb56e228ec1e90957e9779a15c49e64
Thibaut <thibaut.mattio@gmail.com>
2021-09-21 17:03:12 +0200

    Simplify URLs

--- src/ocamlorg_web/lib/url.ml
+++ src/ocamlorg_web/lib/url.ml
@@ -2,53 +2,52 @@ let index = "/"
 
 let history = "/history"
 
-let community_around_web = "/community/aroundweb"
+let community_around_web = "/aroundweb"
 
-let community_events = "/community/events"
+let community_events = "/events"
 
-let community_media_archive = "/community/mediaarchive"
+let community_media_archive = "/mediaarchive"
 
-let community_news = "/community/blog"
+let community_news = "/blog"
 
-let community_news_archive = "/community/newsarchive"
+let community_news_archive = "/newsarchive"
 
-let community_opportunities = "/community/opportunities"
+let community_opportunities = "/opportunities"
 
-let principles_successes = "/principles/successes"
+let principles_successes = "/successes"
 
-let principles_industrial_users = "/principles/users"
+let principles_industrial_users = "/users"
 
-let principles_academic = "/principles/academic"
+let principles_academic = "/academic"
 
-let principles_what_is_ocaml = "/principles/whatisocaml"
+let principles_what_is_ocaml = "/whatisocaml"
 
-let legal_carbon_footprint = "/legal/carbonfootprint"
+let legal_carbon_footprint = "/carbonfootprint"
 
-let legal_privacy = "/legal/privacy"
+let legal_privacy = "/privacy"
 
-let legal_terms = "/legal/terms"
+let legal_terms = "/terms"
 
-let resources_tutorials = "/resources/tutorials"
+let resources_tutorials = "/tutorials"
 
-let resources_install_ocaml =
-  "/resources/tutorials/up-and-running-with-ocaml/#installing-ocaml"
+let resources_tutorial name = "/tutorials/" ^ name
 
-let resources_applications = "/resources/applications"
+let resources_applications = "/applications"
 
-let resources_archive = "/resources/archive"
+let resources_archive = "/archive"
 
-let resources_best_practices = "/resources/bestpractices"
+let resources_best_practices = "/bestpractices"
 
-let resources_developing_in_ocaml = "/resources/developinginocaml"
+let resources_developing_in_ocaml = "/developinginocaml"
 
-let resources_language = "/resources/language"
+let resources_language = "/language"
 
-let resources_papers = "/resources/papers"
+let resources_papers = "/papers"
 
-let resources_papers_archive = "/resources/papersarchive"
+let resources_papers_archive = "/papersarchive"
 
-let resources_platform = "/resources/platform"
+let resources_platform = "/platform"
 
-let resources_releases = "/resources/releases"
+let resources_releases = "/releases"
 
-let resources_using_ocaml = "/resources/usingocaml"
+let resources_using_ocaml = "/usingocaml"
