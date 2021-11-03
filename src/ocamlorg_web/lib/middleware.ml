32410ab794b80f0fb3e8ff9ffb7d77f080ef5c53
Thibaut <thibaut.mattio@gmail.com>
2021-11-03 13:56:26 +0100

    Fix no_trailing_slash middleware
    Co-authored-by: Desire Kaleba <desirekaleba@gmail.com>

--- src/ocamlorg_web/lib/middleware.ml
+++ src/ocamlorg_web/lib/middleware.ml
@@ -1,11 +1,13 @@
 let no_trailing_slash next_handler request =
   let target = Dream.target request in
-  if target = "/" then
+  match target with
+  | "/" ->
     next_handler request
-  else if String.get target (String.length target - 1) = '/' then
-    Dream.redirect request (String.sub target 0 (String.length target - 1))
-  else
-    next_handler request
+  | _ ->
+    if String.ends_with ~suffix:"/" target then
+      Dream.redirect request (String.sub target 0 (String.length target - 1))
+    else
+      next_handler request
 
 let set_locale handler req =
   let language =
--- src/ocamlorg_web/lib/ocamlorg_web.ml
+++ src/ocamlorg_web/lib/ocamlorg_web.ml
@@ -15,5 +15,6 @@ let run () =
   let state = Ocamlorg.Package.init () in
   Dream_cli.run ~debug:Config.debug ~interface:"0.0.0.0" ~port:Config.port
   @@ Dream.logger
+  @@ Middleware.no_trailing_slash
   @@ Router.router state
   @@ Page_handler.not_found
