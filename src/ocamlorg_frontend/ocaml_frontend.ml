36619f3d1c89a46a9bf50f702426b061a1328930
Thibaut <thibaut.mattio@gmail.com>
2021-12-10 15:47:30 +0100

    Add a library for ocaml_frontend

---
+++ src/ocamlorg_frontend/ocaml_frontend.ml
@@ -0,0 +1,65 @@
+module Url = Url
+
+let about () = About.render ()
+
+let academic_users () = Academic_users.render ()
+
+let best_practices () = Best_practices.render ()
+
+let books () = Books.render ()
+
+let community () = Community.render ()
+
+let workshop () = Workshop.render ()
+
+let events () = Events.render ()
+
+let home () = Home.render ()
+
+let industrial_users () = Industrial_users.render ()
+
+let learn () = Learn.render ()
+
+let manual () = Manual.render ()
+
+let blog () = Blog.render ()
+
+let blog_category () = Blog_category.render ()
+
+let opportunities () = Opportunities.render ()
+
+let opportunity () = Opportunity.render ()
+
+let package_overview () = Package_overview.render ()
+
+let package_documentation () = Package_documentation.render ()
+
+
+let package_toplevel () = Package_toplevel.render ()
+
+
+let packages () = Packages.render ()
+
+let packages_search () = Packages_search.render ()
+
+let papers () = Papers.render ()
+
+let problems () = Problems.render ()
+
+let release () = Release.render ()
+
+let releases () = Releases.render ()
+
+let success_stories () = Success_stories.render ()
+
+let success_story () = Success_story.render ()
+
+let tutorial () = Tutorial.render ()
+
+let carbon_footprint () = Carbon_footprint.render ()
+
+let privacy () = Privacy.render ()
+
+let terms () = Terms.render ()
+
+let not_found () = Not_found.render ()
---
+++ src/ocamlorg_frontend/url.ml
@@ -0,0 +1,74 @@
+let index = "/"
+
+let learn = "/learn"
+
+let packages = "/packages"
+
+let packages_search = "/packages/search"
+
+let package v = "/p/" ^ v
+
+let package_with_univ hash v = "/u/" ^ hash ^ "/" ^ v
+
+let package_with_version v version = "/p/" ^ v ^ "/" ^ version
+
+let package_with_hash_with_version hash v version =
+  "/u/" ^ hash ^ "/" ^ v ^ "/" ^ version
+
+let package_toplevel v version = "/p/" ^ v ^ "/" ^ version ^ "/toplevel"
+
+let package_toplevel_with_hash hash v version =
+  "/u/" ^ hash ^ "/" ^ v ^ "/" ^ version ^ "/toplevel"
+
+let package_doc v version page = "/p/" ^ v ^ "/" ^ version ^ "/doc/" ^ page
+
+let package_doc_with_hash hash v version page =
+  "/u/" ^ hash ^ "/" ^ v ^ "/" ^ version ^ "/doc/" ^ page
+
+let community = "/community"
+
+let success_stories = "/success-stories"
+
+let success_story v = "/success-stories/" ^ v
+
+let industrial_users = "/industrial-users"
+
+let academic_users = "/academic-users"
+
+let about = "/about"
+
+let manual = "/manual/index.html"
+
+let manual_with_version v = "/manual/" ^ v
+
+let books = "/books"
+
+let releases = "/releases"
+
+let release v = "/releases/" ^ v
+
+let events = "/events"
+
+let workshop v = "/workshops/" ^ v
+
+let blog = "/blog"
+
+let opportunities = "/opportunities"
+
+let opportunity v = "/opportunities/" ^ v
+
+let carbon_footprint = "/carbon-footprint"
+
+let privacy = "/privacy"
+
+let terms = "/terms"
+
+let papers = "/papers"
+
+let tutorial name = "/learn/" ^ name
+
+let getting_started = tutorial "up-and-running-with-ocaml"
+
+let best_practices = "/learn/best-practices"
+
+let problems = "/problems"
