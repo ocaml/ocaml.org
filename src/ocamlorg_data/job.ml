bbba05bb0fe31ab0813d5e1b365c0ba93bf51540
Thibaut <thibaut.mattio@gmail.com>
2022-02-10 00:34:42 +0100

    Add new opportunities

--- src/ocamlorg_data/job.ml
+++ src/ocamlorg_data/job.ml
@@ -44,6 +44,13 @@ let all =
   ; company_logo = {js|/logos/janestreet.svg|js}
   };
  
+  { title = {js|Developer Educator|js}
+  ; link = {js|https://www.janestreet.com/join-jane-street/position/5596802002/|js}
+  ; location = {js|United States|js}
+  ; company = {js|Jane Street|js}
+  ; company_logo = {js|/logos/janestreet.svg|js}
+  };
+ 
   { title = {js|Senior Software Engineer|js}
   ; link = {js|https://www.marigold.dev/job-offers/software-engineer-senior-1|js}
   ; location = {js|France|js}
@@ -70,5 +77,54 @@ let all =
   ; location = {js|France|js}
   ; company = {js|Nomadic Labs|js}
   ; company_logo = {js|https://research-development.nomadic-labs.com/images/meanwhile_202107/nomadic-logo-new.png|js}
+  };
+ 
+  { title = {js|Computer Scientist|js}
+  ; link = {js|https://frama-c.com/jobs/2022-02-01-permanent-computer-scientist-cyber-security-verification.html|js}
+  ; location = {js|France|js}
+  ; company = {js|CEA|js}
+  ; company_logo = {js|https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/CEA_logo_nouveau.svg/1255px-CEA_logo_nouveau.svg.png|js}
+  };
+ 
+  { title = {js|Fullstack Developer|js}
+  ; link = {js|https://jobs.lever.co/issuu/e80e3f6e-4e2a-4e63-b6df-61f16d4b7209|js}
+  ; location = {js|Denmark|js}
+  ; company = {js|Issuu|js}
+  ; company_logo = {js|https://lever-client-logos.s3.amazonaws.com/84963f7c-5208-4789-813f-59b515174479-1443049024308.png|js}
+  };
+ 
+  { title = {js|Senior Software Engineer|js}
+  ; link = {js|https://cryptosense.com/vacancies/senior-software-engineer|js}
+  ; location = {js|Remote|js}
+  ; company = {js|Cryptosense|js}
+  ; company_logo = {js|https://global-uploads.webflow.com/5fb79f97eb305e5f85f6dff6/5fb7a15497f97810803970a0_CS%20logo_300px.png|js}
+  };
+ 
+  { title = {js|Software Engineer|js}
+  ; link = {js|https://www.simcorp.com/en/career/find-a-job/software-engineer-c-ocamland-apl-r201947|js}
+  ; location = {js|Denmark|js}
+  ; company = {js|SimCorp|js}
+  ; company_logo = {js|https://www.simcorp.com/dist/global/images/simcorp-logo-md.svg|js}
+  };
+ 
+  { title = {js|Formal Methods Engineer|js}
+  ; link = {js|https://trust-in-soft.com/trustinsoft-careers/|js}
+  ; location = {js|United States|js}
+  ; company = {js|TrustInSoft|js}
+  ; company_logo = {js|https://trust-in-soft.com/wp-content/uploads/sites/5/2020/07/TIS-logo-Blck_No-Bckgrd.png|js}
+  };
+ 
+  { title = {js|Js_of_ocaml Developer|js}
+  ; link = {js|https://trust-in-soft.com/trustinsoft-careers/|js}
+  ; location = {js|United States|js}
+  ; company = {js|TrustInSoft|js}
+  ; company_logo = {js|https://trust-in-soft.com/wp-content/uploads/sites/5/2020/07/TIS-logo-Blck_No-Bckgrd.png|js}
+  };
+ 
+  { title = {js|Senior Software Engineer|js}
+  ; link = {js|https://jobs.lever.co/returntocorp/cfd5085f-b8f1-40d8-afab-3e8c70c0a70b|js}
+  ; location = {js|United States|js}
+  ; company = {js|r2c|js}
+  ; company_logo = {js|https://res.cloudinary.com/practicaldev/image/fetch/s--6bcYomzd--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/organization/profile_image/1847/848298ae-97cf-4c28-b4cf-c524dbe68c26.jpg|js}
   }]
 
