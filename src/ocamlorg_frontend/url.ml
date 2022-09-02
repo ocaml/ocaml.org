545a2aa40385d9011941113ee6223784d75747ed
Cuihtlauac ALVARADO <cuihtlauac.alvarado@orange.com>
2022-09-02 14:10:00 +0200

    Link to license, changelog and readme in overview
    Fix https://github.com/ocaml/ocaml.org/issues/36
    
    Display links to change log, readme, license in overview.
    Also displays licence name.

--- src/ocamlorg_frontend/url.ml
+++ src/ocamlorg_frontend/url.ml
@@ -11,6 +11,10 @@ let package_with_hash_with_version hash v version =
 
 let package_doc v version page = "/p/" ^ v ^ "/" ^ version ^ "/doc/" ^ page
 
+let package_readme v version = package_doc v version "README.md.html"
+let package_changes v version = package_doc v version "CHANGES.md.html"
+let package_license v version = package_doc v version "LICENSE.md.html"
+
 let package_doc_with_hash hash v version page =
   "/u/" ^ hash ^ "/" ^ v ^ "/" ^ version ^ "/doc/" ^ page
 
