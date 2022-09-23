cafb57643c1ba9d9382feed6b9dc6560c7d75c9b
Cuihtlauac Alvarado <cuihtlauac@users.noreply.github.com>
2022-09-23 19:13:56 +0200

    Handle HEAD requests using Dream middleware
    Requests found to have a HEAD method are tweaked in two ways:
    
    1. Method is turned into GET
    2. An empty body response is substituted to the GET handler issued one,
    using the same headers but Content-Length, under some circumstances
    
    Content-Length is preserved if present or added if missing. Headers are
    untouched when chunked encoding transfer is used.
    
    Co-authored-by: Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
    Co-authored-by: Thibaut Mattio <thibaut.mattio@gmail.com>

--- src/ocamlorg_web/lib/middleware.ml
+++ src/ocamlorg_web/lib/middleware.ml
@@ -6,3 +6,23 @@ let no_trailing_slash next_handler request =
       if String.ends_with ~suffix:"/" target then
         Dream.redirect request (String.sub target 0 (String.length target - 1))
       else next_handler request
+
+let head handler request =
+  match Dream.method_ request with
+  | `HEAD ->
+      let open Lwt.Syntax in
+      Dream.set_method_ request `GET;
+      let* response = handler request in
+      let transfer_encoding = Dream.header response "Transfer-Encoding" in
+      let* () =
+        if
+          transfer_encoding = Some "chunked"
+          || Dream.has_header response "Content-Length"
+        then Lwt.return_unit
+        else
+          let+ body = Dream.body response in
+          body |> String.length |> string_of_int
+          |> Dream.add_header response "Content-Length"
+      in
+      Dream.empty ~headers:(Dream.all_headers response) (Dream.status response)
+  | _ -> handler request
