551e494441b9c15c1c368596ae28cfbf69785626
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2022-09-23 15:50:29 +0200

    Code formatting

--- src/ocamlorg_web/lib/middleware.ml
+++ src/ocamlorg_web/lib/middleware.ml
@@ -7,17 +7,21 @@ let no_trailing_slash next_handler request =
         Dream.redirect request (String.sub target 0 (String.length target - 1))
       else next_handler request
 
-let head handler request = match Dream.method_ request with
-| `HEAD ->
-    let open Lwt.Syntax in
-    let* response = handler request in
-    let transfer_encoding = Dream.header response "Transfer-Encoding" in
-    let* _ =
-      if transfer_encoding = Some "chunked" || Dream.has_header response "Content-Length" then
-        Lwt.return_unit
-      else
-        let+ body = Dream.body response in
-        body |> String.length |> string_of_int |> Dream.add_header response "Content-Length"
+let head handler request =
+  match Dream.method_ request with
+  | `HEAD ->
+      let open Lwt.Syntax in
+      let* response = handler request in
+      let transfer_encoding = Dream.header response "Transfer-Encoding" in
+      let* _ =
+        if
+          transfer_encoding = Some "chunked"
+          || Dream.has_header response "Content-Length"
+        then Lwt.return_unit
+        else
+          let+ body = Dream.body response in
+          body |> String.length |> string_of_int
+          |> Dream.add_header response "Content-Length"
       in
-    Dream.empty ~headers:(Dream.all_headers response) (Dream.status response)
-| _ -> handler request
+      Dream.empty ~headers:(Dream.all_headers response) (Dream.status response)
+  | _ -> handler request
--- src/ocamlorg_web/lib/router.ml
+++ src/ocamlorg_web/lib/router.ml
@@ -23,71 +23,71 @@ let media_loader =
       let* store in
       Media.last_modified store (Mirage_kv.Key.v path))
 
-let get_and_head pattern handler = [
-  Dream.get pattern handler;
-  Dream.head pattern handler
-]
+let get_and_head pattern handler =
+  [ Dream.get pattern handler; Dream.head pattern handler ]
 
 let page_routes =
   Dream.scope ""
     [ Dream_dashboard.analytics (); Dream_encoding.compress; Middleware.head ]
-    (List.flatten [
-      get_and_head Url.index Handler.index;
-      get_and_head Url.learn Handler.learn;
-      get_and_head Url.platform Handler.platform;
-      get_and_head Url.community Handler.community;
-      get_and_head (Url.success_story ":id") Handler.success_story;
-      get_and_head Url.industrial_users Handler.industrial_users;
-      get_and_head Url.academic_users Handler.academic_users;
-      get_and_head Url.about Handler.about;
-      get_and_head Url.books Handler.books;
-      get_and_head Url.releases Handler.releases;
-      get_and_head (Url.release ":id") Handler.release;
-      get_and_head (Url.workshop ":id") Handler.workshop;
-      get_and_head Url.blog Handler.blog;
-      get_and_head Url.news Handler.news;
-      get_and_head (Url.news_post ":id") Handler.news_post;
-      get_and_head Url.jobs Handler.jobs;
-      get_and_head Url.carbon_footprint Handler.carbon_footprint;
-      get_and_head Url.privacy_policy Handler.privacy_policy;
-      get_and_head Url.governance Handler.governance;
-      get_and_head Url.papers Handler.papers;
-      get_and_head Url.best_practices Handler.best_practices;
-      get_and_head Url.problems Handler.problems;
-      get_and_head (Url.tutorial ":id") Handler.tutorial;
-      get_and_head Url.playground Handler.playground;
-    ])
+    (List.flatten
+       [
+         get_and_head Url.index Handler.index;
+         get_and_head Url.learn Handler.learn;
+         get_and_head Url.platform Handler.platform;
+         get_and_head Url.community Handler.community;
+         get_and_head (Url.success_story ":id") Handler.success_story;
+         get_and_head Url.industrial_users Handler.industrial_users;
+         get_and_head Url.academic_users Handler.academic_users;
+         get_and_head Url.about Handler.about;
+         get_and_head Url.books Handler.books;
+         get_and_head Url.releases Handler.releases;
+         get_and_head (Url.release ":id") Handler.release;
+         get_and_head (Url.workshop ":id") Handler.workshop;
+         get_and_head Url.blog Handler.blog;
+         get_and_head Url.news Handler.news;
+         get_and_head (Url.news_post ":id") Handler.news_post;
+         get_and_head Url.jobs Handler.jobs;
+         get_and_head Url.carbon_footprint Handler.carbon_footprint;
+         get_and_head Url.privacy_policy Handler.privacy_policy;
+         get_and_head Url.governance Handler.governance;
+         get_and_head Url.papers Handler.papers;
+         get_and_head Url.best_practices Handler.best_practices;
+         get_and_head Url.problems Handler.problems;
+         get_and_head (Url.tutorial ":id") Handler.tutorial;
+         get_and_head Url.playground Handler.playground;
+       ])
 
 let package_route t =
   Dream.scope ""
     [ Dream_dashboard.analytics (); Dream_encoding.compress; Middleware.head ]
-    (List.flatten [
-      get_and_head Url.packages (Handler.packages t);
-      get_and_head Url.packages_search (Handler.packages_search t);
-      get_and_head (Url.package ":name") (Handler.package t);
-      get_and_head (Url.package_docs ":name") (Handler.package_docs t);
-      get_and_head (Url.package_with_univ ":hash" ":name") (Handler.package t);
-      get_and_head
-        (Url.package_with_version ":name" ":version")
-        ((Handler.package_versioned t) Handler.Package);
-      get_and_head
-        (Url.package_with_hash_with_version ":hash" ":name" ":version")
-        ((Handler.package_versioned t) Handler.Universe);
-      get_and_head
-        (Url.package_doc ":name" ":version" "**")
-        ((Handler.package_doc t) Handler.Package);
-      get_and_head
-        (Url.package_doc_with_hash ":hash" ":name" ":version" "**")
-        ((Handler.package_doc t) Handler.Universe);
-    ])
+    (List.flatten
+       [
+         get_and_head Url.packages (Handler.packages t);
+         get_and_head Url.packages_search (Handler.packages_search t);
+         get_and_head (Url.package ":name") (Handler.package t);
+         get_and_head (Url.package_docs ":name") (Handler.package_docs t);
+         get_and_head
+           (Url.package_with_univ ":hash" ":name")
+           (Handler.package t);
+         get_and_head
+           (Url.package_with_version ":name" ":version")
+           ((Handler.package_versioned t) Handler.Package);
+         get_and_head
+           (Url.package_with_hash_with_version ":hash" ":name" ":version")
+           ((Handler.package_versioned t) Handler.Universe);
+         get_and_head
+           (Url.package_doc ":name" ":version" "**")
+           ((Handler.package_doc t) Handler.Package);
+         get_and_head
+           (Url.package_doc_with_hash ":hash" ":name" ":version" "**")
+           ((Handler.package_doc t) Handler.Universe);
+       ])
 
 let graphql_route t =
   Dream.scope ""
     [ Dream_encoding.compress; Middleware.head ]
-    (
-      Dream.any "/api" (Dream.graphql Lwt.return (Graphql.schema t)) ::
-      get_and_head "/graphiql" (Dream.graphiql "/api");
-    )
+    (Dream.any "/api" (Dream.graphql Lwt.return (Graphql.schema t))
+    :: get_and_head "/graphiql" (Dream.graphiql "/api"))
 
 let router t =
   Dream.router
@@ -99,8 +99,10 @@ let router t =
       graphql_route t;
       Dream.scope ""
         [ Dream_encoding.compress; Middleware.head ]
-        (List.flatten [ get_and_head "/media/**" (Dream.static ~loader:media_loader "") ]);
+        (List.flatten
+           [ get_and_head "/media/**" (Dream.static ~loader:media_loader "") ]);
       Dream.scope ""
         [ Dream_encoding.compress; Middleware.head ]
-        (List.flatten [ get_and_head "/**" (Dream.static ~loader:asset_loader "") ]);
+        (List.flatten
+           [ get_and_head "/**" (Dream.static ~loader:asset_loader "") ]);
     ]
