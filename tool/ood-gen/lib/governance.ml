ade56bd938b1ebb788a8ae32f123061795be3123
Thibaut Mattio <thibaut.mattio@gmail.com>
2022-11-10 17:57:53 +0100

    Scrape team members

--- tool/ood-gen/lib/governance.ml
+++ tool/ood-gen/lib/governance.ml
@@ -25,8 +25,9 @@ type t = {
 }
 [@@deriving yaml]
 
-let graphql_request =
-  {|
+module Graphql = struct
+  let request =
+    {|
 {
   organization(login: "ocaml") {
     id
@@ -63,6 +64,22 @@ let graphql_request =
 }
 |}
 
+  let xdg = Xdg.create ~env:Sys.getenv_opt ()
+  let home = Xdg.home_dir xdg
+
+  let data () =
+    let open Lwt.Syntax in
+    (* TODO: Find a better way to handle tokens *)
+    let ( / ) = Filename.concat in
+    let token_fpath = home / ".github" / "github-activity-token" in
+    let token = Import.Sys.read_file token_fpath in
+    Lwt_main.run
+    @@ let+ result = Github.request token request in
+       match result with
+       | Ok result -> result
+       | Error (`Msg message) -> failwith message
+end
+
 let all () =
   let s = Data.read "governance.yml" |> Option.get in
   let yaml = Utils.decode_or_raise Yaml.of_string s in
@@ -70,6 +87,49 @@ let all () =
 
 let scrape () =
   let t = all () in
+  let (data : Github.organization) = Graphql.data () in
+  let github_member_to_member (member : Github.member) : member =
+    {
+      name = (match member.name with Some name -> name | None -> member.login);
+      github = member.login;
+      role =
+        (match member.role with
+        | "MAINTAINER" -> "Team Lead"
+        | "MEMBER" | _ -> "Maintainer");
+    }
+  in
+  let fetch_members (team : subteam) =
+    match team.github with
+    | None -> team
+    | Some github_team ->
+        let github_team =
+          List.find
+            (fun (t : Github.team) -> t.Github.name = github_team)
+            data.Github.teams
+        in
+        {
+          team with
+          members =
+            List.map github_member_to_member github_team.members
+            |> List.sort (fun m1 m2 ->
+                   if m1.role = "Team Lead" && m2.role != "Team Lead" then -1
+                   else if m2.role = "Team Lead" && m1.role != "Team Lead" then
+                     1
+                   else String.compare m1.name m2.name);
+        }
+  in
+  let t =
+    {
+      teams =
+        List.map
+          (fun t -> { t with subteams = List.map fetch_members t.subteams })
+          t.teams;
+      working_groups =
+        List.map
+          (fun t -> { t with subteams = List.map fetch_members t.subteams })
+          t.working_groups;
+    }
+  in
   let yaml = Yaml.to_string ~encoding:`Utf8 (to_yaml t) |> Result.get_ok in
   Import.Sys.write_file "data/governance.yml" yaml
 
