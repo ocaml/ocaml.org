e730ef0acc1dab104281b37bd6e4cd529550a980
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2022-12-12 16:19:07 +0100

    Use type aliases for identical types

--- tool/ood-gen/lib/job.ml
+++ tool/ood-gen/lib/job.ml
@@ -8,14 +8,7 @@ type metadata = {
 }
 [@@deriving yaml]
 
-type t = {
-  title : string;
-  link : string;
-  location : string;
-  publication_date : string option;
-  company : string;
-  company_logo : string;
-}
+type t = metadata
 
 let path = Fpath.v "data/jobs.yml"
 
