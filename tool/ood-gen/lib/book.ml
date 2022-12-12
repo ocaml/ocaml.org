6f0b4df65902b1c2a32a6bdfb54ab3b55a08897f
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2022-12-12 14:35:56 +0100

    Inline book metadata
    Replace field Book.t.meta by fieds from Book.metadata

--- tool/ood-gen/lib/book.ml
+++ tool/ood-gen/lib/book.ml
@@ -20,7 +20,20 @@ let parse content =
   let metadata, _ = Utils.extract_metadata_body content in
   metadata_of_yaml metadata
 
-type t = { meta : metadata; body_md : string; body_html : string }
+type t = {
+  title : string;
+  description : string;
+  authors : string list;
+  language : string;
+  published : string;
+  cover : string;
+  isbn : string option;
+  links : link list;
+  rating : int option;
+  featured : bool;
+  body_md : string;
+  body_html : string
+}
 
 let all () =
   Utils.map_files
@@ -28,19 +41,16 @@ let all () =
       let metadata, body = Utils.extract_metadata_body content in
       let metadata = Utils.decode_or_raise metadata_of_yaml metadata in
       {
-        meta =
-          {
-            title = metadata.title;
-            description = metadata.description;
-            authors = metadata.authors;
-            language = metadata.language;
-            published = metadata.published;
-            cover = metadata.cover;
-            isbn = metadata.isbn;
-            links = metadata.links;
-            rating = metadata.rating;
-            featured = metadata.featured;
-          };
+        title = metadata.title;
+        description = metadata.description;
+        authors = metadata.authors;
+        language = metadata.language;
+        published = metadata.published;
+        cover = metadata.cover;
+        isbn = metadata.isbn;
+        links = metadata.links;
+        rating = metadata.rating;
+        featured = metadata.featured;
         body_md = String.trim body;
         body_html = Omd.of_string body |> Omd.to_html;
       })
@@ -71,12 +81,12 @@ let pp ppf v =
   ; body_md = %a
   ; body_html = %a
   }|}
-    Pp.string v.meta.title Pp.string
-    (Utils.slugify v.meta.title)
-    Pp.string v.meta.description (Pp.list Pp.string) v.meta.authors Pp.string
-    v.meta.language Pp.string v.meta.published Pp.string v.meta.cover
-    (Pp.option Pp.string) v.meta.isbn (Pp.list pp_link) v.meta.links
-    (Pp.option Pp.int) v.meta.rating Pp.bool v.meta.featured Pp.string v.body_md
+    Pp.string v.title Pp.string
+    (Utils.slugify v.title)
+    Pp.string v.description (Pp.list Pp.string) v.authors Pp.string
+    v.language Pp.string v.published Pp.string v.cover
+    (Pp.option Pp.string) v.isbn (Pp.list pp_link) v.links
+    (Pp.option Pp.int) v.rating Pp.bool v.featured Pp.string v.body_md
     Pp.string v.body_html
 
 let pp_list = Pp.list pp
