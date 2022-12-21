9449da29d686fc50bc68d037586ac096ce7b5931
Florian Angeletti <florian.angeletti@inria.fr>
2022-12-21 13:48:36 +0100

    OCaml 4.14.1 release (#727)
    * 5.0.0 release : fix date
    
    * OCaml 4.14.1 release
    
    * Sort releases by versions
    
    * review: sort by release with explicit function name
    
    * 4.14.1 release: review
    
    * 4.14.1.md page: add a link to opam

--- tool/ood-gen/lib/release.ml
+++ tool/ood-gen/lib/release.ml
@@ -33,6 +33,10 @@ type t = {
   body_html : string;
 }
 
+let sort_by_decreasing_version x y =
+  let to_list s = List.map int_of_string_opt @@ String.split_on_char '.' s in
+  compare (to_list y.version) (to_list x.version)
+
 let all () =
   Utils.map_files
     (fun content ->
@@ -55,8 +59,7 @@ let all () =
         body_html = Omd.of_string body |> Hilite.Md.transform |> Omd.to_html;
       })
     "releases/"
-  |> List.sort (fun a b -> String.compare a.date b.date)
-  |> List.rev
+  |> List.sort sort_by_decreasing_version
 
 let pp_kind ppf v = Fmt.pf ppf "%s" (match v with `Compiler -> "`Compiler")
 
