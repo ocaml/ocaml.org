ec99b68b3cbca28777aca53e1c2f02f8e4e8e5a0
Sabine Schmaltz <sabine@tarides.com>
2023-01-17 16:44:00 +0100

    minor changes to the playground example

--- playground/src/example.ml
+++ playground/src/example.ml
@@ -4,13 +4,12 @@ let adts =
   {|(* 
   Welcome to OCaml Play, the official OCaml playground!
 
-  You don't need to install anything on your local machine -
-  just write your code here and see the results immediately
-  in the Output panel.
+  You don't need to install anything - just write your code
+  and see the results appear in the Output panel.
 
-  It's also compatible with OCaml 5: you can use it to try
-  the new Domain module and the effect handlers. Below is an
-  example that calculates the Fibonacci sequence in parallel.
+  It's also compatible with OCaml 5: you can use the new
+  Domain module and effect handlers. Below is a naive example
+  that calculates the Fibonacci sequence in parallel.
   
   Happy hacking!
 *)
@@ -32,4 +31,14 @@ let rec fib_par n d =
 let () =
   let res = fib_par n num_domains in
   Printf.printf "fib(%d) = %d\n" n res
+
+(*
+  For a more in-depth, realistic example of how to use
+  parallel computation, take a lot at
+  https://v2.ocaml.org/releases/5.0/manual/parallelism.html#s:par_iterators
+
+  A better, single-threaded implementation that calculates
+  the Fibonacci sequence uses memoization, and you can read about it at
+  https://cs3110.github.io/textbook/chapters/ds/memoization.html#fibonacci
+*)
 |}
