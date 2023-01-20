42b9e834b31ebf70e416c4cd4f017ade5cb85f72
Cuihtlauac Alvarado <cuihtlauac@users.noreply.github.com>
2023-01-20 16:25:10 +0100

    Ood: Remove Unused Archetype Module (#841)
    Remove unused Archetype module
    
    Co-authored-by: Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>

--- tool/ood-gen/lib/meta.ml
+++ tool/ood-gen/lib/meta.ml
@@ -14,30 +14,3 @@ module Proficiency = struct
     | "advanced" -> Ok `Advanced
     | s -> Error (`Msg ("Unknown proficiency type: " ^ s))
 end
-
-module Archetype = struct
-  type t =
-    [ `Beginner
-    | `Teacher
-    | `Library_author
-    | `Application_developer
-    | `End_user
-    | `Distribution_manager ]
-
-  let to_string = function
-    | `Beginner -> "beginner"
-    | `Teacher -> "teacher"
-    | `Library_author -> "library-author"
-    | `Application_developer -> "application-developer"
-    | `End_user -> "end-user"
-    | `Distribution_manager -> "distribution-manager"
-
-  let of_string = function
-    | "beginner" -> Ok `Beginner
-    | "teacher" -> Ok `Teacher
-    | "library-author" -> Ok `Library_author
-    | "application-developer" -> Ok `Application_developer
-    | "end-user" -> Ok `End_user
-    | "distribution-manager" -> Ok `Distribution_manager
-    | s -> Error (`Msg ("Unknown proficiency type: " ^ s))
-end
