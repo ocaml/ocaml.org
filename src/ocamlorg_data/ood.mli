feb2bde39618d6771523b6ee451f24a5e1f8b829
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2023-01-20 13:10:49 +0100

    Remove unused module

--- src/ocamlorg_data/ood.mli
+++ src/ocamlorg_data/ood.mli
@@ -1,18 +1,3 @@
-module Meta : sig
-  module Archetype : sig
-    type t =
-      [ `Application_developer
-      | `Beginner
-      | `Distribution_manager
-      | `End_user
-      | `Library_author
-      | `Teacher ]
-
-    val to_string : t -> string
-    val of_string : string -> (t, [> `Msg of string ]) result
-  end
-end
-
 module Academic_institution : sig
   type location = { lat : float; long : float }
 
