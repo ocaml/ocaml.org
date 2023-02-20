ea81c85fc5e3080cdc5007e55f26a6b843ce4851
Sabine Schmaltz <sabine@tarides.com>
2023-02-20 13:14:17 +0100

    better naming of to/from functions, flatten

--- src/ocamlorg_web/lib/static.ml
+++ src/ocamlorg_web/lib/static.ml
@@ -38,8 +38,8 @@ let not_modified ~last_modified request =
 
 let loader ~read ~digest ?(not_cached = []) local_root path request =
   let not_cached = List.mem path not_cached in
-  let static_url = Static_url.Digest_url.of_string path in
-  let filepath = Static_url.Digest_url.to_path static_url in
+  let static_url = Static_url.of_url_path path in
+  let filepath = Static_url.to_file_path static_url in
   let result = read local_root filepath in
   match result with
   | None -> Handler.not_found request
--- src/ocamlorg_web/lib/static_url.ml
+++ src/ocamlorg_web/lib/static_url.ml
@@ -1,27 +1,26 @@
-module Digest_url = struct
-  type t = { filepath : string; digest : string option }
+type t = { filepath : string; digest : string option }
 
-  let of_path ~digest filepath = { filepath; digest }
-  let to_path static_url = static_url.filepath
+let of_file_path ~digest filepath = { filepath; digest }
+let to_file_path static_url = static_url.filepath
 
-  let to_string static_url =
-    match static_url.digest with
-    | None -> static_url.filepath
-    | Some digest -> Fmt.str "/_/%s/%s" digest static_url.filepath
+let to_url_path static_url =
+  match static_url.digest with
+  | None -> static_url.filepath
+  | Some digest -> Fmt.str "/_/%s/%s" digest static_url.filepath
 
-  let of_string path =
-    let xs = String.split_on_char '/' path in
-    match xs with
-    | [] -> raise (Invalid_argument "invalid Digest_url: path cannot be empty")
-    | "_" :: [] | [ "_"; _ ] ->
-        raise
-          (Invalid_argument
-             "invalid Digest_url: paths starting with '_' must be followed by \
-              a digest and filepath")
-    | "_" :: x :: xs -> { digest = Some x; filepath = String.concat "/" xs }
-    | _ -> { digest = None; filepath = path }
-end
+let of_url_path path =
+  let xs = String.split_on_char '/' path in
+  match xs with
+  | [] -> raise (Invalid_argument "invalid Digest_url: path cannot be empty")
+  | "_" :: [] | [ "_"; _ ] ->
+      raise
+        (Invalid_argument
+           "invalid Digest_url: paths starting with '_' must be followed by a \
+            digest and filepath")
+  | "_" :: x :: xs -> { digest = Some x; filepath = String.concat "/" xs }
+  | _ -> { digest = None; filepath = path }
 
+(* render a digest URL for a given asset, for use in templates *)
 let asset_digest_url filepath =
   let digest = Asset.hash filepath in
   if digest = None then
@@ -31,7 +30,7 @@ let asset_digest_url filepath =
             "ERROR: '%s' is rendered via asset_digest_url, but it is not an \
              asset!"
             filepath));
-  Digest_url.to_string
-    (Digest_url.of_path
+  to_url_path
+    (of_file_path
        ~digest:(Option.map (fun d -> Dream.to_base64url d) digest)
        filepath)
