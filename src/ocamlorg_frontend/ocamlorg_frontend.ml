e244a1346b520894135a93838f15e9c55e1818ea
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2023-02-22 17:57:44 +0100

    Streamline

--- src/ocamlorg_frontend/ocamlorg_frontend.ml
+++ src/ocamlorg_frontend/ocamlorg_frontend.ml
@@ -4,58 +4,32 @@ module Navmap = Navmap
 module Toc = Toc
 include Package_intf
 
-let about () = About.render ()
-let academic_users users = Academic_users.render users
-let best_practices best_practices = Best_practices.render best_practices
-let books books = Books.render books
-let community ~workshops ~meetups = Community.render ~workshops ~meetups
-let workshop ~videos workshop = Workshop.render ~videos workshop
-let home () = Home.render ()
-
-let industrial_users ~users ~success_stories =
-  Industrial_users.render ~users ~success_stories
-
-let learn ~papers ~release ~books = Learn.render ~papers ~release ~books
-let platform tools = Platform.render tools
-
-let blog ~featured ~rss ~rss_page ~rss_pages_number ~news =
-  Blog.render ~featured ~rss ~rss_page ~rss_pages_number ~news
-
-let news ~page ~pages_number news = News.render ~page ~pages_number news
-let news_post news = News_post.render news
-let jobs ?location ~locations jobs = Jobs.render ?location ~locations jobs
-
-let package_overview ~documentation_status ~readme ~readme_title ~dependencies
-    ~rev_dependencies ~conflicts ~homepages ~source ~changes_filename
-    ~license_filename package =
-  Package_overview.render ~documentation_status ~readme ~readme_title
-    ~dependencies ~rev_dependencies ~conflicts ~homepages ~source
-    ~changes_filename ~license_filename package
-
-let package_documentation ~path ~toc ~maptoc ~content package =
-  Package_documentation.render ~path ~toc ~maptoc ~content package
-
-let package_documentation_not_found ~is_latest_url ~path ~package =
-  Package_documentation_not_found.render ~is_latest_url ~path ~package
-
-let packages stats = Packages.render stats
-let packages_search ~total packages = Packages_search.render ~total packages
-
-let packages_autocomplete_fragment packages =
-  Packages_autocomplete_fragment.render packages
-
-let papers ?search ~recommended_papers papers =
-  Papers.render ?search ~recommended_papers papers
-
-let problems problems = Problems.render problems
-let release release = Release.render release
-let releases ?search releases = Releases.render ?search releases
+let about = About.render
+let academic_users = Academic_users.render
+let best_practices = Best_practices.render
+let books = Books.render
+let community = Community.render
+let workshop = Workshop.render
+let home = Home.render
+let industrial_users = Industrial_users.render
+let learn = Learn.render
+let platform  = Platform.render
+let blog = Blog.render
+let news = News.render
+let news_post = News_post.render
+let jobs = Jobs.render
+let package_overview = Package_overview.render
+let package_documentation = Package_documentation.render
+let package_documentation_not_found = Package_documentation_not_found.render
+let packages  = Packages.render
+let packages_search = Packages_search.render
+let packages_autocomplete_fragment = Packages_autocomplete_fragment.render
+let papers = Papers.render
+let problems = Problems.render
+let release = Release.render
+let releases = Releases.render
 let success_story = Success_story.render
 let tutorial = Tutorial.render
-
-let page ~title ~description ~meta_title ~meta_description ~content ~canonical =
-  Page.render ~title ~description ~meta_title ~meta_description ~content
-    ~canonical
-
-let playground () = Playground.render ()
-let not_found () = Not_found.render ()
+let page = Page.render
+let playground = Playground.render
+let not_found = Not_found.render
