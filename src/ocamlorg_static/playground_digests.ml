819a2dbaaca6b9528181dc6e366b94c0c0e83d2f
Sabine Schmaltz <sabine@tarides.com>
2023-03-01 14:56:59 +0100

    static-file-digest emits formatted code, clean up

--- src/ocamlorg_static/playground_digests.ml
+++
@@ -1,6 +0,0 @@
-let digest filepath = match filepath with
-  | {js|merlin.min.js|js} -> Some {js|{€ñ58Rœn‘düv[uW|js}
-  | {js|playground.min.js|js} -> Some {js|Ô˜½˜;'i1¾É`å¿™Èö|js}
-  | {js|worker.min.js|js} -> Some {js|”J_ÜJæ~7ˆ´“œ5¯|js}
-  | _ -> None
-
