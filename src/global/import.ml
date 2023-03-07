adbe113a8f2d7579db7f9924ce94a4a8d27c0dda
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2023-03-07 11:53:26 +0100

    Revert useless changes, cont'd

--- src/global/import.ml
+++ src/global/import.ml
@@ -4,9 +4,9 @@ module String = struct
 
   let contains_s s1 s2 =
     try
-      let len = length s2 in
-      for i = 0 to length s1 - len do
-        if sub s1 i len = s2 then raise Exit
+      let len = String.length s2 in
+      for i = 0 to String.length s1 - len do
+        if String.sub s1 i len = s2 then raise Exit
       done;
       false
     with Exit -> true
