d12cadaa1fee35fbd55caf6f7fb4b9eaff120671
sabine <sabine@users.noreply.github.com>
2023-03-17 14:10:19 +0100

    package.version is either a `Specific` version or `Latest` (mostly a refactor) (#949)
    This patch
    
    - renames and refactors Ocamlorg_frontend.Package_intf to Ocamlorg_frontend.Package,
      which has a notion of whether we are viewing this package through a 'latest`-URL
      or a specific-version-URL and
    - adds latest_version to Ocamlorg_frontend.Package, so that we can display the
      specific latest version in the version switcher dropdown
    - renders "latest" entry in the version dropdown always next to the specific latest version.

--- src/ocamlorg_frontend/package.ml
+++ src/ocamlorg_frontend/package.ml
@@ -1,15 +1,31 @@
+type version = Latest | Specific of string
+
 type package = {
   name : string;
   description : string;
   license : string;
-  version : string;
+  version : version;
   versions : string list;
+  latest_version : string;
   tags : string list;
   rev_deps : string list;
   authors : Ood.Opam_user.t list;
   maintainers : Ood.Opam_user.t list;
   publication : float;
 }
+
+let specific_version package =
+  match package.version with
+  | Latest -> package.latest_version
+  | Specific v -> v
+
+let render_version package =
+  match package.version with
+  | Latest -> Fmt.str "%s (latest)" package.latest_version
+  | Specific v -> v
+
+let url_version package =
+  match package.version with Latest -> None | Specific v -> Some v
 
 type packages_stats = {
   nb_packages : int;
