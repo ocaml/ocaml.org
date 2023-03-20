0800935ffeea5b9cad1e6b88991d459fcb4a9cef
sabine <sabine@users.noreply.github.com>
2023-03-20 18:11:10 +0100

    Revert "Ood: Yet Another Yaml Processor (#869)" (#988)
    This reverts commit 66e004464814a83151ffc1d401540e33960e08df.

--- tool/ood-gen/lib/job.ml
+++ tool/ood-gen/lib/job.ml
@@ -8,11 +8,19 @@ type metadata = {
 }
 [@@deriving of_yaml, show { with_path = false }]
 
-type t = metadata [@@deriving of_yaml, show { with_path = false }]
+type t = metadata [@@deriving show { with_path = false }]
+
+let decode s =
+  let yaml = Utils.decode_or_raise Yaml.of_string s in
+  match yaml with
+  | `O [ ("jobs", `A xs) ] ->
+      Ok (List.map (Utils.decode_or_raise metadata_of_yaml) xs)
+  | _ -> Error (`Msg "expected a list of jobs")
 
 let all () =
   let job_date j = Option.value ~default:"1970-01-01" j.publication_date in
-  Utils.yaml_sequence_file metadata_of_yaml "jobs.yml"
+  let content = Data.read "jobs.yml" |> Option.get in
+  Utils.decode_or_raise decode content
   |> List.sort (fun j1 j2 -> compare (job_date j2) (job_date j1))
 
 let template () =
