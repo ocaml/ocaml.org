e32c486cbc2b21c6a0fbf1f17dac7df344c7ff01
sabine <sabine@users.noreply.github.com>
2023-03-30 16:07:39 +0200

    Fix 404 pages (#1034)
    * use let</>?
    
    * fmt

--- src/ocamlorg_web/lib/static.ml
+++ src/ocamlorg_web/lib/static.ml
@@ -36,40 +36,35 @@ let not_modified ~last_modified request =
   | None -> false
   | Some date -> String.equal date last_modified
 
-let loader ~read ~digest ?(not_cached = []) local_root path request =
+let loader ~read ~digest ?(not_cached = []) local_root path _request =
   let open Lwt.Syntax in
+  let open Handler in
   let not_cached = List.mem path not_cached in
   let maybe_static_file = Ocamlorg_static.of_url_path path in
-  match maybe_static_file with
-  | None -> Dream.not_found request
-  | Some static_file -> (
-      let filepath = static_file.filepath in
-      let* result = read local_root filepath in
-      match result with
-      | None -> Dream.not_found request
-      | Some asset when not_cached ->
-          Dream.respond
-            ~headers:
-              ([ ("Cache-Control", "no-store, max-age=0") ]
-              @ Dream.mime_lookup path)
-            asset
-      | Some asset ->
-          let digest = digest local_root filepath in
-          if
-            static_file.digest <> None
-            && not (Option.equal ( = ) digest static_file.digest)
-          then
-            Dream.log "asset %s exists but digest does not match: %s != %s"
-              filepath
-              (Option.value ~default:"" static_file.digest)
-              (Dream.to_base64url (Option.value ~default:"" digest));
+  let</>? static_file = maybe_static_file in
+  let filepath = static_file.filepath in
+  let* result = read local_root filepath in
+  let</>? asset = result in
+  if not_cached then
+    Dream.respond
+      ~headers:
+        ([ ("Cache-Control", "no-store, max-age=0") ] @ Dream.mime_lookup path)
+      asset
+  else
+    let digest = digest local_root filepath in
+    if
+      static_file.digest <> None
+      && not (Option.equal ( = ) digest static_file.digest)
+    then
+      Dream.log "asset %s exists but digest does not match: %s != %s" filepath
+        (Option.value ~default:"" static_file.digest)
+        (Dream.to_base64url (Option.value ~default:"" digest));
 
-          let cache_control =
-            match static_file.digest with
-            | None -> "max-age=86400" (* one day *)
-            | Some _ -> "max-age=31536000, immutable"
-          in
-          Dream.respond
-            ~headers:
-              ([ ("Cache-Control", cache_control) ] @ Dream.mime_lookup path)
-            asset)
+    let cache_control =
+      match static_file.digest with
+      | None -> "max-age=86400" (* one day *)
+      | Some _ -> "max-age=31536000, immutable"
+    in
+    Dream.respond
+      ~headers:([ ("Cache-Control", cache_control) ] @ Dream.mime_lookup path)
+      asset
