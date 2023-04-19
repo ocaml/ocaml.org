fecfd15d82638b50d5a1dbfd1a0e1a6344d8abb4
Thibaut Mattio <thibaut.mattio@gmail.com>
2023-04-19 12:09:19 +0200

    Use Yoshi to generate OCaml modules from YAML files (#1051)
    * Vendor yoshi
    * Replace watch.yml data generation with Yoshi

---
+++ tool/yoshi/test/option.t/run.t
@@ -0,0 +1,2 @@
+  $ yoshi -c config.yaml -o generated_output.ml input.yaml
+  $ diff -u output.ml generated_output.ml
---
+++ tool/yoshi/test/simple.t/output.ml
@@ -0,0 +1,10 @@
+type t = {
+  title: string;
+  author: string;
+  duration: int;
+}
+
+let videos_list = [
+  { title = "Introduction to OCaml"; author = "Jane Doe"; duration = 120 };
+  { title = "Functional Programming in OCaml"; author = "John Smith"; duration = 180 }
+]
