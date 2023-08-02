c2117580eb81d6b285602e8e4a8587669fc07a0b
Guillaume Petiot <guillaume@tarides.com>
2023-08-02 11:52:58 +0100

    Make playground/gen_stdlib.sh mac compatible

--- playground/gen_stdlib.sh
+++ playground/gen_stdlib.sh
@@ -3,12 +3,14 @@
 
 mkdir -p stdlib
 
+SWITCH=playground
+SRC_DIR=$HOME/.opam/$SWITCH/lib/ocaml
+
 copylibcmis()  (
   tmpfile=$(mktemp)
-  srcdir=$(opam var $1:lib)
   jsoo_listunits -o $tmpfile $2
   for i in $(cat $tmpfile); do
-    cp $srcdir/?${i#?}.cmi stdlib/ 
+    cp $SRC_DIR/?${i#?}.cmi stdlib/
   done
   rm $tmpfile
 )
@@ -20,6 +22,5 @@ copylibcmis ocaml stdlib
 EXTRA="std_exit unix/unix unix/unixLabels compiler-libs/topdirs"
 
 for i in $EXTRA; do
-  cp $(opam var ocaml:lib)/$i.cmi stdlib/
+  cp $SRC_DIR/$i.cmi stdlib/
 done
-
