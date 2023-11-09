efdc0a205226c67a376bf8180a61450e20942bb3
oyenuga17 <64274826+oyenuga17@users.noreply.github.com>
2023-11-09 20:45:37 +0100

    refactor cmarkit implementation

--- tool/ood-gen/lib/exercise.ml
+++ tool/ood-gen/lib/exercise.ml
@@ -20,21 +20,26 @@ type metadata = {
 }
 [@@deriving of_yaml]
 
-let get_title (h: Cmarkit.Block.Heading.t) = 
+let get_title (h : Cmarkit.Block.Heading.t) =
   let title =
     Cmarkit.Inline.to_plain_text ~break_on_soft:false
-      (Cmarkit.Block.Heading.inline h) in
-    String.concat "\n" (List.map (String.concat "") title)
-  
+      (Cmarkit.Block.Heading.inline h)
+  in
+  String.concat "\n" (List.map (String.concat "") title)
+
 let split_statement_statement (blocks : Cmarkit.Block.t list) =
   let rec blocks_until_heading acc = function
     | [] -> (List.rev acc, [])
-    | (h) :: rest when Cmarkit.Block.Heading.level h = 1 -> (List.rev acc, rest)
+    | Cmarkit.Block.Heading (h, _) :: rest
+      when Cmarkit.Block.Heading.level h = 1 ->
+        (List.rev acc, rest)
     | el :: rest -> blocks_until_heading (el :: acc) rest
   in
   let rec skip_non_heading_blocks = function
     | [] -> []
-    | (h) :: rest when Cmarkit.Block.Heading.level h = 1 -> rest
+    | Cmarkit.Block.Heading (h, _) :: rest
+      when Cmarkit.Block.Heading.level h = 1 ->
+        rest
     | _ :: rest -> skip_non_heading_blocks rest
   in
   let err =
@@ -42,10 +47,10 @@ let split_statement_statement (blocks : Cmarkit.Block.t list) =
      top-level headings: \"Solution\" and \"Statement\""
   in
   match skip_non_heading_blocks blocks with
-  | (h) :: rest when get_title h = "Solution" rest -> (
+  | Cmarkit.Block.Heading (h, _) :: rest when get_title h = "Solution" -> (
       let solution_blocks, rest = blocks_until_heading [] rest in
       match rest with
-      | (h) :: rest when get_title h = "Statement" rest -> (
+      | Cmarkit.Block.Heading (h, _) :: rest when get_title h = "Statement" -> (
           let statements_blocks, rest = blocks_until_heading [] rest in
           match rest with
           | [] -> (statements_blocks, solution_blocks)
@@ -69,10 +74,24 @@ type t = {
 let decode (_, (head, body)) =
   let metadata = metadata_of_yaml head in
   let statement_blocks, solution_blocks =
-    split_statement_statement (Cmarkit.Doc.of_string body)
+    split_statement_statement [ Cmarkit.Doc.block (Cmarkit.Doc.of_string body) ]
   in
-  let statement = Omd.to_html statement_blocks in
-  let solution = Omd.to_html solution_blocks in
+  let statement_blocks_to_doc =
+    Cmarkit.Block.Blocks (statement_blocks, Cmarkit.Meta.none)
+    |> Cmarkit.Doc.make
+  in
+  let solution_blocks_to_doc =
+    Cmarkit.Block.Blocks (solution_blocks, Cmarkit.Meta.none)
+    |> Cmarkit.Doc.make
+  in
+
+  let statement =
+    Cmarkit_html.of_doc ~safe:false
+      (Hilite.Md.transform statement_blocks_to_doc)
+  in
+  let solution =
+    Cmarkit_html.of_doc ~safe:false (Hilite.Md.transform solution_blocks_to_doc)
+  in
   Result.map (of_metadata ~statement ~solution) metadata
 
 let all () = Utils.map_files decode "exercises/*.md"
