6d5043374721c4ef68a43dd7feb32a9d2da80f1b
Shakthi Kannan <author@shakthimaan.com>
2023-12-19 21:29:14 +0530

    Add route for /manuals

--- src/ocamlorg_web/lib/router.ml
+++ src/ocamlorg_web/lib/router.ml
@@ -116,6 +116,9 @@ let router t =
       sitemap_routes;
       Dream.scope ""
         [ Dream_encoding.compress ]
+        [ Dream.get "/manuals/**" (Dream.static "manuals") ];
+      Dream.scope ""
+        [ Dream_encoding.compress ]
         [ Dream.get "/media/**" (Dream.static ~loader:media_loader "") ];
       Dream.scope ""
         [ Dream_encoding.compress ]
