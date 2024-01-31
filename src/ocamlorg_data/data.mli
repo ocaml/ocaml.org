eea2e1f618a3aa71951cf48f4122784492719ec6
sabine <sabine@users.noreply.github.com>
2024-01-31 13:56:16 +0100

    add a rule to crunch the cookbook directory

--- src/ocamlorg_data/data.mli
+++ src/ocamlorg_data/data.mli
@@ -527,7 +527,7 @@ module Cookbook : sig
     title : string;
     problem : string;
     category : string;
-    libraries : string list;
+    packages : string list;
     body_html : string;
   }
 
