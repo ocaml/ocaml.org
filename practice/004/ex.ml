3fb036beb37799dfa6351ec4b37bc645486e136a
Ozyugoo <stellaozy22@gmail.com>
2024-03-13 08:49:19 +0100

    Chore: Add return value and end of line character

--- practice/004/ex.ml
+++ practice/004/ex.ml
@@ -7,10 +7,10 @@ end
 
 module Make(Tested: Testable) : sig val v : test end = struct
   let v = "length" >::: [
-    "nil" >:: (fun _ -> assert_equal (Tested.length []));
-    "cons" >:: (fun _ -> assert_equal (Tested.length ["a"; "b"; "c"]));
+    "nil" >:: (fun _ -> assert_equal 0 (Tested.length []));
+    "cons" >:: (fun _ -> assert_equal 3 (Tested.length ["a"; "b"; "c"]));
   ]
 end
 
 module Work : Testable = Work.Impl
-module Answer : Testable = Answer.Impl
+module Answer : Testable = Answer.Impl"
