630d12348b7a48dd7239e4c02375373b90852725
Divyanka Chaudhari <60309618+divyankachaudhari@users.noreply.github.com>
2024-03-14 17:58:34 +0530

    Added replication factor 1 test case

--- practice/015/ex.ml
+++ practice/015/ex.ml
@@ -17,7 +17,7 @@ module Make(Tested: Testable) : sig val v : test end = struct
         (Tested.replicate ["a"; "b"; "c"] 0));
     "replication factor of 1" >:: (fun _ ->
       assert_equal ["a"; "b"; "c"]
-        (Tested.replicate ["a"; "b"; "c"] 1));  (* This is the new test case *)
+        (Tested.replicate ["a"; "b"; "c"] 1));
   ]
 
   let v = "Replicate the elements of a list a given number of times." >::: [
