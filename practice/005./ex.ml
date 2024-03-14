b4e5e25b8a671854f887bff11033dce437cc6e34
MissJae <jennifer.njoku@gmail.com>
2024-03-13 21:34:46 -0400

    Turn exercise 005 into practice

---
+++ practice/005./ex.ml
@@ -0,0 +1,14 @@
+open OUnit2
+
+module type Testable = sig
+  val rev : 'a list -> 'a list
+end
+
+module Make(Tested: Testable) : sig val v : test end = struct
+  let v = "rev" >::: [
+    "cons" >:: (fun _ -> assert_equal ["c"; "b"; "a"] (Tested.rev ["a"; "b"; "c"]));
+  ]
+end
+
+module Work : Testable = Work.Impl
+module Answer : Testable = Answer.Impl
---
+++ practice/005./work/impl.ml
@@ -0,0 +1,1 @@
+let rev _ = failwith "Not yet implemented"
