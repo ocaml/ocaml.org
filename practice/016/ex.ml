ff58dfeb420112019bafb75e07b0c14ecbf278c1
Divyanka Chaudhari <60309618+divyankachaudhari@users.noreply.github.com>
2024-03-14 18:00:17 +0530

    Added test cases for drop at 0 and 1

--- practice/016/ex.ml
+++ practice/016/ex.ml
@@ -8,13 +8,19 @@ module Make(Tested: Testable) : sig val v : test end = struct
   let tests = "drop" >::: [
     "drop every 3rd from list" >:: (fun _ ->
       assert_equal ["a"; "b"; "d"; "e"; "g"; "h"; "j"]
-        (Tested.drop ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 3));
+        (Tested.drop ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 3)),
     "drop with empty list" >:: (fun _ ->
       assert_equal []
-        (Tested.drop [] 3));
+        (Tested.drop [] 3)),
     "drop with n larger than list length" >:: (fun _ ->
       assert_equal ["a"; "b"]
-        (Tested.drop ["a"; "b"] 3));
+        (Tested.drop ["a"; "b"] 3)),
+    "drop with n equal to 0 (edge case)" >:: (fun _ ->
+      assert_equal ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"]
+        (Tested.drop ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 0)),
+    "drop with n equal to 1" >:: (fun _ ->
+      assert_equal []
+        (Tested.drop ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 1))
   ]
 
   let v = "Drop Every N'th Element Tests" >::: [
