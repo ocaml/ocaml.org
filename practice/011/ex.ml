7ee9aac622c8dbfd3e539bfe32cdcd256866dfa6
Divyanka Chaudhari <60309618+divyankachaudhari@users.noreply.github.com>
2024-03-14 15:02:56 +0530

    Turn exercise 011 into practice (#2196)

---
+++ practice/011/ex.ml
@@ -0,0 +1,29 @@
+open OUnit2
+
+type 'a rle =
+  | One of 'a
+  | Many of int * 'a
+
+module type Testable = sig
+  type 'a rle = One of 'a | Many of int * 'a
+  val encode : 'a list -> 'a rle list
+end
+
+module Make(Tested: Testable) : sig val v : test end = struct
+  open Tested
+  
+  let example_tests = "encode" >::: [
+    "single elements" >:: (fun _ ->
+      assert_equal [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d"; Many (4, "e")]
+        (encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"]));
+    "empty list" >:: (fun _ ->
+      assert_equal [] (encode []));
+  ]
+  
+  let v = "Modified Run-Length Encoding" >::: [
+    example_tests;
+  ]
+end
+
+module Work : Testable = Work.Impl
+module Answer : Testable = Answer.Impl
