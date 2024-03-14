d67bc5da3105923ad6d2aa6d1662287ee83f0ba1
Divyanka Chaudhari <60309618+divyankachaudhari@users.noreply.github.com>
2024-03-14 18:18:40 +0530

    Turned exercise 018 into practice.

---
+++ practice/018/answer/impl.ml
@@ -0,0 +1,9 @@
+let rec fold_until f acc n = function
+  | [] -> (acc, [])
+  | h :: t as l -> if n = 0 then (acc, l)
+                   else fold_until f (f acc h) (n - 1) t
+
+let slice list i k =
+  let _, list = fold_until (fun _ _ -> []) [] i list in
+  let taken, _ = fold_until (fun acc h -> h :: acc) [] (k - i + 1) list in
+  List.rev taken
