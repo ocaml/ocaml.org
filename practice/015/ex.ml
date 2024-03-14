8d512e129a8306a33e2f6a91506720df900e8afc
Divyanka Chaudhari <60309618+divyankachaudhari@users.noreply.github.com>
2024-03-14 17:29:58 +0530

    Turned Exercise 015 into Practice.

---
+++ practice/015/ex.ml
@@ -0,0 +1,26 @@
+open OUnit2
+
+module type Testable = sig
+  val replicate : 'a list -> int -> 'a list
+end
+
+module Make(Tested: Testable) : sig val v : test end = struct
+  let tests = "replicate" >::: [
+    "list with replication" >:: (fun _ ->
+      assert_equal ["a"; "a"; "a"; "b"; "b"; "b"; "c"; "c"; "c"]
+        (Tested.replicate ["a"; "b"; "c"] 3));
+    "empty list" >:: (fun _ ->
+      assert_equal []
+        (Tested.replicate [] 3));
+    "zero replication" >:: (fun _ ->
+      assert_equal []
+        (Tested.replicate ["a"; "b"; "c"] 0));
+  ]
+
+  let v = "Replicate the elements of a list a given number of times." >::: [
+    tests
+  ]
+end
+
+module Work : Testable = Work.Impl
+module Answer : Testable = Answer.Impl
