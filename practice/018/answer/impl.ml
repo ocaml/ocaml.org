226e986c3ae4397fdb253e8ae0fd8e18ffa6c069
Cuihtlauac Alvarado <cuihtlauac@users.noreply.github.com>
2024-03-18 20:20:50 +0100

    Update practice/018/answer/impl.ml

--- practice/018/answer/impl.ml
+++ practice/018/answer/impl.ml
@@ -1,15 +1,2 @@
-let rec fold_until f acc n = function
-  | [] -> (acc, [])
-  | h :: t as l -> if n = 0 then (acc, l)
-                   else fold_until f (f acc h) (n - 1) t
-
-let slice list i k =
-  let _, list = fold_until (fun _ _ -> []) [] i list in
-  let taken, _ = fold_until (fun acc h -> h :: acc) [] (k - i + 1) list in
-  List.rev taken
-(* 
-An elegant way to do this would be to use List.filteri function:
-
 let slice list i k = 
   List.filteri (fun index _ -> index >= i && index <= k) list
-*)
