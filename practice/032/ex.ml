d51d4c6bbd895015311ed30f851b67e93ef9af62
Ozyugoo <stellaozy22@gmail.com>
2024-03-18 18:17:20 +0100

    Modify: Turn exercise 032 to practice

--- practice/032/ex.ml
+++ practice/032/ex.ml
@@ -6,8 +6,8 @@ end
 
 module Make(Tested: Testable) : sig val v : test end = struct
   let v = "gcd" >::: [
-    "nil" >:: (fun _ -> assert_equal 13 (Tested.gcd 13 0));
-    "cons" >:: (fun _ -> assert_equal 2 (Tested.gcd 20536 7826));
+    "gcd_13_0" >:: (fun _ -> assert_equal 13 (Tested.gcd 13 0));
+    "gcd_20536_7826" >:: (fun _ -> assert_equal 2 (Tested.gcd 20536 7826));
   ]
 end
 
