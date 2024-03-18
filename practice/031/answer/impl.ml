61b37ebfad6498df257637f7a360fe2349f50eee
Ozyugoo <stellaozy22@gmail.com>
2024-03-18 18:09:28 +0100

    Modify Turn exercise 031 to practice

--- practice/031/answer/impl.ml
+++ practice/031/answer/impl.ml
@@ -1,5 +1,7 @@
 let is_prime n =
-    let n = abs n in
-    let rec is_not_divisor d =
-      d * d > n || (n mod d <> 0 && is_not_divisor (d + 1)) in
-    n <> 1 && is_not_divisor 2
+  let n = abs n in
+  let rec is_not_divisor d =
+    d * d > n || (n mod d <> 0 && is_not_divisor (d + 1)) in
+  n <> 1 && is_not_divisor 2
+
+  let prime_0_to_100 = [ 2; 3; 5; 7; 11; 13; 17; 19; 23; 29; 31; 37; 41; 43; 47; 53; 59; 61; 67; 71; 73; 79; 83; 89; 97]
--- practice/031/ex.ml
+++ practice/031/ex.ml
@@ -6,9 +6,30 @@ end
 
 module Make(Tested: Testable) : sig val v : test end = struct
   let v = "is_prime" >::: [
-    "nil" >:: (fun _ -> assert_equal false (Tested.(not (is_prime 7))));
-    "cons" >:: (fun _ -> assert_equal true (Tested.is_prime 7));
-  ]
+    "is_prime 2" >:: (fun _ -> assert_equal true (Tested.is_prime 2));
+    "is_prime 3" >:: (fun _ -> assert_equal true (Tested.is_prime 3));
+    "is_prime 5" >:: (fun _ -> assert_equal true (Tested.is_prime 5));
+    "is_prime 7" >:: (fun _ -> assert_equal true (Tested.is_prime 7));
+    "is_prime 11" >:: (fun _ -> assert_equal true (Tested.is_prime 11));
+    "is_prime 13" >:: (fun _ -> assert_equal true (Tested.is_prime 13));
+    "is_prime 17" >:: (fun _ -> assert_equal true (Tested.is_prime 17));
+    "is_prime 19" >:: (fun _ -> assert_equal true (Tested.is_prime 19));
+    "is_prime 23" >:: (fun _ -> assert_equal true (Tested.is_prime 23));
+    "is_prime 29" >:: (fun _ -> assert_equal true (Tested.is_prime 29));
+    "is_prime 31" >:: (fun _ -> assert_equal true (Tested.is_prime 31));
+    "is_prime 37" >:: (fun _ -> assert_equal true (Tested.is_prime 37));
+    "is_prime 41" >:: (fun _ -> assert_equal true (Tested.is_prime 41));
+    "is_prime 43" >:: (fun _ -> assert_equal true (Tested.is_prime 43));
+    "is_prime 47" >:: (fun _ -> assert_equal true (Tested.is_prime 47));
+    "is_prime 53" >:: (fun _ -> assert_equal true (Tested.is_prime 53));
+    "is_prime 59" >:: (fun _ -> assert_equal true (Tested.is_prime 59));
+    "is_prime 61" >:: (fun _ -> assert_equal true (Tested.is_prime 61));
+    "is_prime 73" >:: (fun _ -> assert_equal true (Tested.is_prime 73));
+    "is_prime 79" >:: (fun _ -> assert_equal true (Tested.is_prime 79));
+    "is_prime 83" >:: (fun _ -> assert_equal true (Tested.is_prime 83));
+    "is_prime 89" >:: (fun _ -> assert_equal true (Tested.is_prime 89));
+    "is_prime 97" >:: (fun _ -> assert_equal true (Tested.is_prime 97))
+]
 end
 
 module Work : Testable = Work.Impl
