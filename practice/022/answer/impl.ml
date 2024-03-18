0af098b3902ec185dc5617df77bb9053bb4534f5
Divyanka Chaudhari <60309618+divyankachaudhari@users.noreply.github.com>
2024-03-18 21:04:12 +0530

    Added tail recursive answer in answer2 folder

--- practice/022/answer/impl.ml
+++ practice/022/answer/impl.ml
@@ -3,15 +3,3 @@ let range a b =
     if a > b then [] else a :: aux (a + 1) b
   in
   if a > b then List.rev (aux b a) else aux a b
-
-(* 
-Tail recursive Implementation
-
-let range a b =
-  let rec aux acc high low =
-    if high >= low then
-      aux (high :: acc) (high - 1) low
-    else acc
-  in
-  if a < b then aux [] b a else List.rev (aux [] a b)
-*)
