d1236d4b00d4c2b8beeaf47fac7383a5e49c8068
Ozyugoo <stellaozy22@gmail.com>
2024-03-18 18:57:08 +0100

    Modify: Turn exercise 028 to practice

--- practice/028/ex.ml
+++ practice/028/ex.ml
@@ -6,8 +6,8 @@ end
 
 module Make(Tested: Testable) : sig val v : test end = struct
   let v = "length_sort" >::: [
-    "nil" >:: (fun _ -> assert_equal [[]] (Tested.length_sort [[]]));
-    "cons" >:: (fun _ -> assert_equal [["o"]; ["d"; "e"]; ["d"; "e"]; ["m"; "n"]; ["a"; "b"; "c"]; ["f"; "g"; "h"];
+    "empty_list" >:: (fun _ -> assert_equal [[]] (Tested.length_sort [[]]));
+    "length_sort" >:: (fun _ -> assert_equal [["o"]; ["d"; "e"]; ["d"; "e"]; ["m"; "n"]; ["a"; "b"; "c"]; ["f"; "g"; "h"];
     ["i"; "j"; "k"; "l"]] (Tested.length_sort [["a"; "b"; "c"]; ["d"; "e"]; ["f"; "g"; "h"]; ["d"; "e"];
     ["i"; "j"; "k"; "l"]; ["m"; "n"]; ["o"]]));
   ]
