3454edd7f4111bb806b2de5d6c4dfb5d9ad9988f
Appleeyes <appleeyespackage@gmail.com>
2024-03-19 12:45:47 +0100

    fix build

--- practice/061A/ex.ml
+++ practice/061A/ex.ml
@@ -6,10 +6,14 @@ module type Testable = sig
 end
 
 module Make(Tested: Testable) : sig val v : test end = struct
-  let v = "count_leaves" >::: [
-    "empty_tree" >:: (fun _ -> assert_equal 0 (Tested.count_leaves Empty));
-    "single_leaf" >:: (fun _ -> assert_equal 1 (Tested.count_leaves (Node(1, Empty, Empty))));
+  open Tested
+  
+  let tests = [
+    "empty_tree", (fun _ -> assert_equal 0 (count_leaves Empty));
+    "single_leaf", (fun _ -> assert_equal 1 (count_leaves (Node(1, Empty, Empty))));
   ]
+
+  let v = "count_leaves" >::: List.map (fun (name, _) -> name >:: fun _ -> ()) tests
 end
 
 module Work : Testable = Work.Impl
