2a716ea389948ba1f7a765306136c3943662b3ed
Appleeyes <appleeyespackage@gmail.com>
2024-03-19 17:09:05 +0100

    fix build

--- practice/067/answer/impl.ml
+++ practice/067/answer/impl.ml
@@ -1,13 +1,16 @@
+type 'a binary_tree =
+  | Empty
+  | Node of 'a * 'a binary_tree * 'a binary_tree
+
 let rec string_of_tree = function
   | Empty -> ""
   | Node(data, l, r) ->
      let data = String.make 1 data in
      match l, r with
      | Empty, Empty -> data
-     | _, _ -> data ^ "(" ^ (string_of_tree l)
-               ^ "," ^ (string_of_tree r) ^ ")"
+     | _, _ -> data ^ "(" ^ (string_of_tree l) ^ "," ^ (string_of_tree r) ^ ")"
 
-let tree_of_string =
+let tree_of_string s =
   let rec make ofs s =
     if ofs >= String.length s || s.[ofs] = ',' || s.[ofs] = ')' then
       (Empty, ofs)
@@ -16,7 +19,7 @@ let tree_of_string =
       if ofs + 1 < String.length s && s.[ofs + 1] = '(' then
         let l, ofs = make (ofs + 2) s in (* skip "v(" *)
         let r, ofs = make (ofs + 1) s in (* skip "," *)
-          (Node (v, l, r), ofs + 1) (* skip ")" *)
+        (Node (v, l, r), ofs + 1) (* skip ")" *)
       else (Node (v, Empty, Empty), ofs + 1)
   in
-    fun s -> fst (make 0 s)
+  fst (make 0 s)
--- practice/067/ex.ml
+++ practice/067/ex.ml
@@ -1,27 +1,27 @@
 open OUnit2
 
 module type Testable = sig
-  val string_of_tree : 'a binary_tree -> string
-  val tree_of_string : string -> 'a binary_tree
+  type 'a binary_tree = | Empty | Node of 'a * 'a binary_tree * 'a binary_tree
+  val string_of_tree : char binary_tree -> string
+  val tree_of_string : string -> char binary_tree
 end
 
-module Make(Tested: Testable) : sig val v : test end = struct
-  let v = "string_of_tree & tree_of_string" >::: [
-    "test_string_of_tree_works_correctly" >:: (fun _ ->
-        let open BinaryTree in
+module Make(Tested: Testable) : sig val v : OUnit2.test end = struct
+  open Tested
+  
+  let v = "string_of_tree and tree_of_string" >::: [
+    "test_string_of_tree" >:: (fun _ ->
         let example_tree =
-          Node ('a', Node ('b', Node ('d', Empty, Empty), Node ('e', Empty, Empty)),
-               Node ('c', Empty, Node ('f', Node ('g', Empty, Empty), Empty))) in
-        let expected_string = "a(b(d,e),c(,f(g,)))" in
-        assert_equal expected_string (Tested.string_of_tree example_tree));
-
-    "test_tree_of_string_works_correctly" >:: (fun _ ->
-        let open BinaryTree in
+          Node ('a', Node ('b', Node ('d', Empty, Empty), Node ('e', Empty, Empty)), Node ('c', Empty, Node ('f', Node ('g', Empty, Empty), Empty)))
+        in
+        assert_equal ~printer:(fun x -> x) "a(b(d,e),c(,f(g,)))" (string_of_tree example_tree));
+    
+    "test_tree_of_string" >:: (fun _ ->
         let example_string = "a(b(d,e),c(,f(g,)))" in
         let expected_tree =
-          Node ('a', Node ('b', Node ('d', Empty, Empty), Node ('e', Empty, Empty)),
-               Node ('c', Empty, Node ('f', Node ('g', Empty, Empty), Empty))) in
-        assert_equal expected_tree (Tested.tree_of_string example_string))
+          Node ('a', Node ('b', Node ('d', Empty, Empty), Node ('e', Empty, Empty)), Node ('c', Empty, Node ('f', Node ('g', Empty, Empty), Empty)))
+        in
+        assert_equal ~printer:(fun x -> string_of_tree x) expected_tree (tree_of_string example_string));
   ]
 end
 
