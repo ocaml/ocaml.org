4e5d2615146177301583f7ea5e0cc60648bc6440
Appleeyes <appleeyespackage@gmail.com>
2024-03-19 01:44:03 +0100

    implemented Turn-exercise-061A-into-practice

--- practice/061A/answer/impl.ml
+++ practice/061A/answer/impl.ml
@@ -1,5 +1,5 @@
 let rec count_leaves = function
     | Empty -> 0
     | Node (_, Empty, Empty) -> 1
-    | Node (_, l, r) -> count_leaves l + count_leaves r;
+    | Node (_, l, r) -> count_leaves l + count_leaves r
     
