475ab3195fcf2610ce2ed49743c66acc53ec5408
Mahalakshmi <mahalaxmidev.1019@gmail.com>
2024-03-19 16:43:35 -0400

    Update practice/049/ex.ml
    Co-authored-by: Cuihtlauac Alvarado <cuihtlauac@users.noreply.github.com>

--- practice/049/ex.ml
+++ practice/049/ex.ml
@@ -10,11 +10,11 @@ module Make(Tested: Testable) : sig val v : test end = struct
 
 let example_tests = "gray" >::: [
   "n = 1" >:: (fun _ ->
-    assert_equal ["0"; "1"] (gray 1));
+    assert_equal ["0"; "1"] (Tested.gray 1));
   "n = 2" >:: (fun _ ->
-    assert_equal ["00"; "01"; "11"; "10"] (gray 2));
+    assert_equal ["00"; "01"; "11"; "10"] (Tested.gray 2));
   "n = 3" >:: (fun _ ->
-    assert_equal ["000"; "001"; "011"; "010"; "110"; "111"; "101"; "100"] (gray 3));
+    assert_equal ["000"; "001"; "011"; "010"; "110"; "111"; "101"; "100"] (Tested.gray 3));
 ]
 let v = "Gray Code Tests" >::: [example_tests]
 end
