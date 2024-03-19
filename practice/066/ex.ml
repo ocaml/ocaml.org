3babfe3675a023dd77627af592206ccc7a9a7003
Appleeyes <appleeyespackage@gmail.com>
2024-03-19 03:14:29 +0100

    implemented Turn-exercise-066-into-practice

---
+++ practice/066/ex.ml
@@ -0,0 +1,22 @@
+open OUnit2
+
+module type Testable = sig
+  val layout_binary_tree_3 : 'a binary_tree -> ('a * int * int) binary_tree
+end
+
+module Make(Tested: Testable) : sig val v : test end = struct
+  let v = "layout_binary_tree_3" >::: [
+    "test_layout_binary_tree_3_works_correctly" >:: (fun _ ->
+        let open BinaryTree in
+        let open Layout in
+        let example_layout_tree =
+          Node ('a', Node ('b', Empty, Empty), Node ('c', Empty, Empty)) in
+        let expected_layout =
+          Node (('a', 2, 1), Node (('b', 1, 2), Empty, Empty), Node (('c', 3, 2), Empty, Empty)) in
+        assert_equal expected_layout (Tested.layout_binary_tree_3 example_layout_tree));
+  ]
+end
+
+module Work : Testable = Work.Impl
+
+module Answer : Testable = Answer.Impl
---
+++ practice/066/work/impl.ml
@@ -0,0 +1,1 @@
+let layout_binary_tree_3 _ = failwith "Not yet implemented"
