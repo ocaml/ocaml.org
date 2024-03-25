93271481df631174ccc1dfab95650f8cdfd3aa56
Ozyugoo <stellaozy22@gmail.com>
2024-03-25 13:15:42 +0100

    Update Turn exercise 031 to practice

--- practice/031/ex.ml
+++ practice/031/ex.ml
@@ -5,17 +5,24 @@ module type Testable = sig
 end
 
 module Make(Tested: Testable) : sig val v : test end = struct
-  let rec flag_prime n lst_n =
-    match lst_n with 
-    | [] -> []
-    | hd :: tl -> (hd, is_prime tl) :: flag_prime n tl
+  let rec flag_primes n = function
+  | [] -> []
+  | p :: u ->
+    if n = p then 
+      (p, Tested.is_prime p) :: flag_primes (n + 1) u
+    else
+      (n, false) :: flag_primes (n + 1) (p :: u)
 
   let primes = [2; 3; 5; 7; 11; 13; 17; 19; 23; 29; 31; 37; 41; 43; 47; 53; 59; 61; 67; 71; 73; 79; 83; 89; 97]
 
-  let tests = "flag_prime" >::: List.map (fun (n, is_prime) ->
-        let test_name = Printf.sprintf "Testing if %d is prime" n in
-        test_name >:: (fun _ -> assert_equal is_prime (Tested.is_prime n))
-      ) primes
+  let prime_flags = flag_primes 2 primes
+
+  let tests =
+  "flag_prime" >::: List.map (fun n ->
+    let is_prime = Tested.is_prime n in
+    let test_name = Printf.sprintf "Testing if %d is prime" n in
+    test_name >:: (fun _ -> assert_equal is_prime (Tested.is_prime n))
+  ) primes
 
   let v = "is_prime Tests" >::: [
     tests
