08665f818ca4d4d0486f7aac3629171b5a28c4ab
maha-sachin <mahasachin108@gmail.com>
2024-03-25 12:25:28 -0400

    added more test case height 2 and 3

--- practice/059/ex.ml
+++ practice/059/ex.ml
@@ -13,15 +13,44 @@ module Make(Tested: Testable) : sig val v : test end = struct
     "hbal_tree" >::: [
       "Test Empty Tree" >:: (fun _ ->
         let trees = hbal_tree 0 in
-        let expected_result = [Empty] in
-        assert_equal expected_result trees
+        let expected_trees = [Empty] in
+        assert_equal expected_trees trees
       );
 
       "Test Single Node Tree" >:: (fun _ ->
         let trees = hbal_tree 1 in
-        let expected_result = [Node ('x', Empty, Empty)] in
-        assert_equal expected_result trees
-      );   
+        let expected_trees = [Node ('x', Empty, Empty)] in
+        assert_equal expected_trees trees
+      ); 
+
+      "Test Height 2 Tree" >:: (fun _ ->
+        let trees = hbal_tree 2 in
+        let expected_trees = [
+          Node ('x', Node ('x', Empty, Empty), Node ('x', Empty, Empty));
+          Node ('x', Node ('x', Empty, Empty), Empty);
+          Node ('x', Empty, Node ('x', Empty, Empty))
+        ] in
+        assert_equal expected_trees trees;
+      );
+
+      "Test Height 3 Tree" >:: (fun _ ->
+        let trees = hbal_tree 3 in
+        let expected_trees = [
+          Node ('x', Node ('x', Empty, Empty), Node ('x', Empty, Node ('x', Empty, Empty)));
+          Node ('x', Node ('x', Empty, Node ('x', Empty, Empty)), Node ('x', Empty, Empty));
+          Node ('x', Node ('x', Empty, Empty), Node ('x', Node ('x', Empty, Empty), Empty));
+          Node ('x', Node ('x', Empty, Empty), Node ('x', Empty, Node ('x', Empty, Empty)));
+          Node ('x', Node ('x', Empty, Node ('x', Empty, Empty)), Node ('x', Node ('x', Empty, Empty), Empty));
+          Node ('x', Node ('x', Empty, Node ('x', Empty, Empty)), Node ('x', Empty, Node ('x', Empty, Empty)));
+          Node ('x', Empty, Node ('x', Empty, Node ('x', Empty, Empty)));
+          Node ('x', Node ('x', Empty, Empty), Node ('x', Node ('x', Empty, Empty), Node ('x', Empty, Empty)));
+          Node ('x', Node ('x', Empty, Node ('x', Empty, Empty)), Node ('x', Node ('x', Empty, Empty), Node ('x', Empty, Empty)));
+          Node ('x', Node ('x', Empty, Empty), Node ('x', Empty, Node ('x', Empty, Empty)));
+          Node ('x', Node ('x', Empty, Empty), Node ('x', Node ('x', Empty, Empty), Empty));
+          Node ('x', Node ('x', Empty, Empty), Node ('x', Empty, Node ('x', Empty, Empty)));
+        ] in
+        assert_equal expected_trees trees;
+      )
     ]
   let v = "Height-Balanced Binary Trees" >::: [test_hbal_tree ()]
 end
