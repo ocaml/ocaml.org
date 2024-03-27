93ba63ce4029cc96b00e24273206b29a003c1ad3
mnaibei <muchajulius@gmail.com>
2024-03-27 10:51:29 +0300

    Fix dune syntax error

--- practice/003/answer/impl.ml
+++ practice/003/answer/impl.ml
@@ -1,4 +1,3 @@
-# let rec at k = function
-    | [] -> None
-    | h :: t -> if k = 0 then Some h else at (k - 1) t;;
-val at : int -> 'a list -> 'a option = <fun>
+let rec at k = function
+  | [] -> None
+  | h :: t -> if k = 0 then Some h else at (k - 1) t
