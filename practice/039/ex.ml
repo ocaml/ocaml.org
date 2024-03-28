9121816e2158a3c7b46c0a8a861c20bf1d7884e5
jahielkomu <ktumuhairwe24@gmail.com>
2024-03-28 23:33:25 +0300

    Updating the checking of primes in practice/039

--- practice/039/ex.ml
+++ practice/039/ex.ml
@@ -13,18 +13,21 @@ module Make(Tested: Testable) : sig val v : test end = struct
     let rec is_not_divisor d =
       d * d > n || (n mod d <> 0 && is_not_divisor (d + 1))
     in
-      is_not_divisor 2
-
-  let rec prime_bool_list start_num end_num =
-    let rec primes list n =
-      if n > end_num then list else
-        primes ((n, is_prime n) :: list) (n + 1)
-    in
-    primes [] start_num
+    is_not_divisor 2
+  
+  let rec generate_primes n acc upper_limit =
+    if n > upper_limit then acc
+    else generate_primes (n + 1) ((n, is_prime n) :: acc) upper_limit
+  
+  let primes_list = generate_primes 2 [] 7920
+  
+  let tests =
+    "all_primes" >::: List.map (fun (p, is_prime) ->
+      let prime_test = Printf.sprintf "Testing if %d is prime" p in
+      prime_test >:: (fun _ -> assert_equal is_prime (List.mem p (Tested.all_primes 2 7920)))
+    ) primes_list
      let v = "List_primes" >::: [
-    "all_primes" >:::List.map (fun (prime_bool_list) -> (fun _ ->
-      assert_equal 1000 (List.length (prime_bool_list( all_primes 2 7920)))));
-
+    tests
   ]
 end
 
