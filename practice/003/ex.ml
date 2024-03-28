a56f0414ce2866f1a737c14fec762212a7ea4e3e
Mucha Naibei <muchajulius@gmail.com>
2024-03-28 10:00:55 +0300

    Turn exercise 003 into practice (#2284)
    * Turn exercise 003 into practice
    
    * Remove .DS_Store file
    
    * Fix dune syntax error
    
    * Fix dune error

---
+++ practice/003/ex.ml
@@ -0,0 +1,25 @@
+open OUnit2
+
+module type Testable = sig
+  val last : 'a list -> 'a option
+end
+
+module Make(Tested: Testable) : sig val v : test end = struct
+  let v = "last" >::: [
+    "nil" >:: (fun _ -> assert_equal None (Tested.last []));
+    "cons" >:: (fun _ -> assert_equal (Some "d") (Tested.last ["a"; "b"; "c"; "d"]));
+  ]
+end
+
+module Work : Testable = struct
+  let last lst = match List.rev lst with
+    | [] -> None
+    | h :: _ -> Some h
+end
+
+module Answer : Testable = struct
+  let last lst = match List.rev lst with
+    | [] -> None
+    | h :: _ -> Some h
+end
+
