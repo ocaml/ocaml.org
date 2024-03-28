886ee6b29d1fbbccd7d81652c6eac81e9ea9710b
Patrick Ferris <patrick@sirref.org>
2024-03-28 17:15:13 +0100

    Add Outreachy winter 2023 round (#2244)
    * Add Outreachy winter 2023 round
    
    * line editing
    
    * Convert Outreachy descriptions to HTML from markdown
    
    * Update data/outreachy.yml
    
    ---------
    
    Co-authored-by: Christine Rose <christinerose@users.noreply.github.com>
    Co-authored-by: Cuihtlauac Alvarado <cuihtlauac@users.noreply.github.com>

--- tool/ood-gen/lib/outreachy.ml
+++ tool/ood-gen/lib/outreachy.ml
@@ -1,4 +1,4 @@
-type project = {
+type project_metadata = {
   title : string;
   description : string;
   mentee : string;
@@ -9,14 +9,44 @@ type project = {
 }
 [@@deriving of_yaml, show { with_path = false }]
 
-type metadata = { name : string; projects : project list }
+type project = {
+  title : string;
+  description_html : string;
+  mentee : string;
+  blog : string option;
+  source : string;
+  mentors : string list;
+  video : string option;
+}
+[@@deriving of_yaml, show { with_path = false }]
+
+type metadata = { name : string; projects : project_metadata list }
 [@@deriving of_yaml, show { with_path = false }]
 
 type t = { name : string; projects : project list }
-[@@deriving stable_record ~version:metadata, show { with_path = false }]
+[@@deriving
+  stable_record ~version:metadata ~modify:[ projects ],
+    show { with_path = false }]
 
 let of_metadata m = of_metadata m
-let decode s = Result.map of_metadata (metadata_of_yaml s)
+
+let modify_projects : project_metadata list -> project list =
+ fun v ->
+  List.map
+    (fun (p : project_metadata) ->
+      {
+        title = p.title;
+        description_html =
+          Cmarkit.Doc.of_string p.description |> Cmarkit_html.of_doc ~safe:true;
+        mentee = p.mentee;
+        blog = p.blog;
+        source = p.source;
+        mentors = p.mentors;
+        video = p.video;
+      })
+    v
+
+let decode s = Result.map (of_metadata ~modify_projects) (metadata_of_yaml s)
 let all () = Utils.yaml_sequence_file ~key:"rounds" decode "outreachy.yml"
 
 let template () =
@@ -24,7 +54,7 @@ let template () =
     {|
 type project =
   { title : string
-  ; description : string
+  ; description_html : string
   ; mentee : string
   ; blog : string option
   ; source : string
