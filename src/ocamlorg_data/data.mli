2d547d59cc172c1e62a55550b366216a53e50c5a
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2024-04-03 13:53:41 +0200

    Added description field to tasks

--- src/ocamlorg_data/data.mli
+++ src/ocamlorg_data/data.mli
@@ -527,7 +527,7 @@ module Cookbook : sig
     subcategories : category list;
   }
 
-  type task = { title : string; slug : string; category : category }
+  type task = { title : string; slug : string; category : category; description : string }
   type code_block_with_explanation = { code : string; explanation : string }
   type package = { name : string; version : string }
 
