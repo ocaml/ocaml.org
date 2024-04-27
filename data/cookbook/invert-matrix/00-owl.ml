2223de7d89a491cfc5aeeb3b021447206a574e4d
Frédéric LOYER <frederic.loyer@club-internet.fr>
2024-04-27 12:23:13 +0200

    Cookbook : linalg

---
+++ data/cookbook/invert-matrix/00-owl.ml
@@ -0,0 +1,16 @@
+---
+packages:
+- name: owl
+  tested_version: 1.1
+  used_libraries:
+  - owl
+---
+let m = Owl.Mat.of_array [|1.;2.;3.;4.|] 2 2
+let minv = Owl.Linalg.D.inv m
+
+let () =
+Owl.Mat.iteri_2d (fun i j a -> Printf.printf "(%i,%i) %.1f\n" i j a) minv;;
+
+(* A verification *)
+
+let () = assert Owl.Mat.( m *@ minv =~ eye 2)
---
+++ data/cookbook/normalize-vector/00-owl.ml
@@ -0,0 +1,16 @@
+---
+packages:
+- name: owl
+  tested_version: 1.1
+  used_libraries:
+  - owl
+---
+let v = Owl.Mat.of_array [|1.;1.|] 2 1
+
+let norm = Owl.Linalg.D.norm v
+
+let v' = Owl.Mat.( v /$ norm );;
+
+let () =
+  Owl.Mat.iteri_2d (fun i j a -> Printf.printf "(%i) %.1f\n" i a) v';;
+
