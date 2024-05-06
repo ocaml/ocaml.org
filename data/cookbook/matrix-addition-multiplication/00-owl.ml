06f50f2540d546d5e247164556373bb39606e685
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2024-05-06 19:12:20 +0200

    Cookbook linakg\n\nRecreate PR #2386

---
+++ data/cookbook/matrix-addition-multiplication/00-owl.ml
@@ -0,0 +1,18 @@
+---
+packages:
+- name: owl
+  tested_version: "1.1"
+  used_libraries:
+  - owl
+---
+let m1 = Owl.Mat.of_array [|1.;2.;3.;4.|] 2 2
+let m2 = Owl.Mat.of_array [|5.;6.;7.;8.|] 2 2
+
+let add = Owl.Mat.( m1 + m2 );;
+let mul = Owl.Mat.( m1 *@ m2 );;
+
+let () =
+  Owl.Mat.iteri_2d (fun i j a -> Printf.printf "(%i,%i) %.1f\n" i j a) add;;
+let () =
+  Owl.Mat.iteri_2d (fun i j a -> Printf.printf "(%i,%i) %.1f\n" i j a) mul;;
+
