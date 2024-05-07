1a94c8c85f7fd88cbf6b4684fb82c6d70664557b
gpopides <gpopides@pm.me>
2024-05-07 22:04:01 +0200

    swap them

--- data/cookbook/serialise-to-json/00-serialize-yojson.ml
+++ data/cookbook/serialise-to-json/00-serialize-yojson.ml
@@ -8,20 +8,11 @@ packages:
 (* We need to open Ppx_yojson_conv_lib.Yojson_conv.Primitives in order to deserialize primitives*)
 open Ppx_yojson_conv_lib.Yojson_conv.Primitives
 
-let json = {|
-{
-  "name": "ocaml",
-  "url": "https://ocaml.org/"
-}
-|}
-
 type language = { name: string; url: string } [@@deriving yojson]
 
 (* First we convert the json string to Yojson.Safe.t and then we use the generated function to create the record *)
 (* If the parsing fails the result will be None. Otherwise it will be the deserialized record*)
-try
-  let result = Yojson.Safe.from_string json |> language_of_yojson 
-  Some result
-with Ppx_yojson_conv_lib__Yojson_conv.Of_yojson_error (exception, _) ->
-  let _ = Printexc.to_string exception print_endline in
-  None
+let ocaml_language = { name: "ocaml"; url: "https://ocaml.org/" }
+
+(* We convert the language to a Yojson.Safe.T and the convert that to a string, which in the end we print*)
+let _ = yojson_of_language |> Yojson.Safe.to_string |> print_endline
