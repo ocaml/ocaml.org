05588b0635e69d360fae8c99901b74ced83c1c9f
Cuihtlauac Alvarado <cuihtlauac@users.noreply.github.com>
2024-05-16 17:03:38 +0200

    chore: Sort data source alphabetically (#2435)
    Co-authored-by: Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>

--- tool/ood-gen/bin/gen.ml
+++ tool/ood-gen/bin/gen.ml
@@ -2,33 +2,33 @@ open Cmdliner
 
 let term_templates =
   [
-    ("is_ocaml_yet", Ood_gen.Is_ocaml_yet.template);
     ("academic_institution", Ood_gen.Academic_institution.template);
     ("book", Ood_gen.Book.template);
+    ("changelog", Ood_gen.Changelog.template);
+    ("changelog_feed", Ood_gen.Changelog.ChangelogFeed.create_feed);
+    ("code_examples", Ood_gen.Code_example.template);
+    ("cookbook", Ood_gen.Cookbook.template);
+    ("event", Ood_gen.Event.template);
+    ("exercises", Ood_gen.Exercise.template);
     ("governance", Ood_gen.Governance.template);
+    ("industrial_user", Ood_gen.Industrial_user.template);
+    ("is_ocaml_yet", Ood_gen.Is_ocaml_yet.template);
     ("job", Ood_gen.Job.template);
     ("news", Ood_gen.News.template);
-    ("changelog", Ood_gen.Changelog.template);
-    ("industrial_user", Ood_gen.Industrial_user.template);
+    ("opam_user", Ood_gen.Opam_user.template);
     ("outreachy", Ood_gen.Outreachy.template);
+    ("pages", Ood_gen.Page.template);
     ("paper", Ood_gen.Paper.template);
-    ("exercises", Ood_gen.Exercise.template);
+    ("planet", Ood_gen.Planet.template);
+    ("planet_feed", Ood_gen.Planet.GlobalFeed.create_feed);
     ("release", Ood_gen.Release.template);
     ("resource", Ood_gen.Resource.template);
     ("success_story", Ood_gen.Success_story.template);
     ("tool", Ood_gen.Tool.template);
     ("tutorial", Ood_gen.Tutorial.template);
-    ("workshops", Ood_gen.Workshop.template);
     ("video", Ood_gen.Video.template);
-    ("planet", Ood_gen.Planet.template);
-    ("planet_feed", Ood_gen.Planet.GlobalFeed.create_feed);
-    ("changelog_feed", Ood_gen.Changelog.ChangelogFeed.create_feed);
     ("watch", Ood_gen.Watch.template);
-    ("opam_user", Ood_gen.Opam_user.template);
-    ("pages", Ood_gen.Page.template);
-    ("code_examples", Ood_gen.Code_example.template);
-    ("event", Ood_gen.Event.template);
-    ("cookbook", Ood_gen.Cookbook.template);
+    ("workshops", Ood_gen.Workshop.template);
   ]
 
 let cmds =
