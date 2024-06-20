6f1d3502910644e9221b4b0a7fa6721c92c80473
sabine <6594573+sabine@users.noreply.github.com>
2024-06-20 12:26:43 +0200

    some editing

--- data/cookbook/sorting-lists-and-arrays/00-stdlib.ml
+++ data/cookbook/sorting-lists-and-arrays/00-stdlib.ml
@@ -2,17 +2,21 @@
 packages: []
 ---
 
-(* Sorting a list of item, returning a sorted copy. *)
+(* Sorting a list of `int` values, returning a sorted copy, using the integer `compare` function.
+
+  **Note:** Different types have different compare functions, you need to look up the one for the type of values you're sorting. *)
 let l = [ 1; 90; 42; 27 ]
 let l' = List.sort compare a
 
-(* Sorting an array while modifying it. *)
+(* Sorting an array while modifying it in place. *)
 let a = [| 1; 90; 42; 27 |]
 let () = Array.sort compare a
 
 (* 
-Defining a custom `compare` function (here, a case insensitive string comparison) and using it to compare.
- Note: the comparison function applied to `a` and `b` should return 1 if `a` is greater, 0 if equal, and -1 if lower than `b`.
+  Defining a custom `compare` function (here a case insensitive string comparison) and using it to sort an array.
+
+  **Note:** the comparison function applied to `a` and `b` needs to return `1` if `a` is
+  greater than `b`, `0`, if they're equal, and `-1` if `a` is less than `b`.
 *)
 let compare_insensitive a b = 
     compare (String.lowercase_ascii a) (String.lowercase_ascii b)
