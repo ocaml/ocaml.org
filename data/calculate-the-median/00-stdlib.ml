8eb5d58b555d0540e9f509551e8605c2244fca3f
Christine Rose <christinerose@users.noreply.github.com>
2024-06-25 02:45:42 -0700

    minor formatting

--- data/calculate-the-median/00-stdlib.ml
+++ data/calculate-the-median/00-stdlib.ml
@@ -3,7 +3,7 @@ packages: []
 discussion: |
   - **Description:** The [Median](https://statistics.laerd.com/statistical-guides/measures-central-tendency-mean-mode-median.php) measure of central tendency
   is the middle value in an ordered set of data. It is useful for statistical analysis as it isn't easily influenced by extreme values (outliers)
-  - **Data Assumption:** This code assumes the data is of type float. While we can calculate the median for any numeric value, it makes sense to use floats in Ocaml since all
+  - **Data Assumption:** This code assumes the data is of type float. While we can calculate the median for any numeric value, it makes sense to use floats in OCaml since all
   integers i.e 1 can be represented as floats 1. but not all floats can be represented by integer data types i.e. 1.5 does not equal 1 or 2
   - **Note:** When there is an even number of elements in the data, the median will be the average of the two middle values.
 ---
@@ -11,7 +11,7 @@ discussion: |
 (* `median` returns the middle value of the elements of `data`.
     An empty list will return `invalid_arg "empty list"`.
 
-    To find the median, the data needs to be sorted. `List.sort` comes with the Ocaml standard library 
+    To find the median, the data needs to be sorted. `List.sort` comes with the OCaml standard library 
     and is used to create the `sorted_data` object.
 
     We then check if we have an even or odd number of elements in `data`. This determines how we find the middle value.     
