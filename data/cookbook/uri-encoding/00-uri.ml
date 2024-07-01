01bf13c873b170c98d21379bdf35223bcc8e6007
Christine Rose <christinerose@users.noreply.github.com>
2024-07-01 02:26:42 -0700

    an to a

--- data/cookbook/uri-encoding/00-uri.ml
+++ data/cookbook/uri-encoding/00-uri.ml
@@ -21,15 +21,15 @@ let () =
   assert (Uri.to_string
            = "https://login:password@ocaml.org:8080/search?param1=a,b&param2=d%C3%A9j%C3%A0%20#anchor")
 
-(* An different approach is to start with a known URI and change some of its components.
+(* A different approach is to start with a known URI and change some of its components.
 
   The following functions are available: `with_scheme`, `with_userinfo`,
   `with_password`, `with_port`, `with_path`, `with_query`, and `with_fragment`.
 
-  Except for `with_path` and `with_query`, all of them take an `option` type parameter,
+  Except for `with_path` and `with_query`, all of them take an `option` type parameter
   in order to allow removing that specific component from the URI.
 
-  Note, these functions have a first parameter, which is the URI to modify,
+  Note: these functions have a first parameter, which is the URI to modify,
   and a second that contains the corresponding field's new value. *)
 let uri = Uri.of_string "https://ocaml.org/"
 
