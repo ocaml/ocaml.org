dfb0c48aed50090b4f0e37ed30ec0811c3df840b
sabine <6594573+sabine@users.noreply.github.com>
2024-07-02 15:34:18 +0200

    mention ppx_regexp first

--- data/cookbook/regex-misc/01-ppx_regexp.ml
+++ data/cookbook/regex-misc/01-ppx_regexp.ml
@@ -1,13 +1,13 @@
 ---
 packages:
-- name: "re"
-  tested_version: "1.10.4"
-  used_libraries:
-  - re
 - name: "ppx_regexp"
   tested_version: "0.5.1"
   used_libraries:
   - ppx_regexp
+- name: "re"
+  tested_version: "1.10.4"
+  used_libraries:
+  - re
 discussion: |
   - **Understanding `re`:** The `re` library proposes multiple advantages over the `Str` library, which is shipped with OCaml. It supports multiple syntaxes, and its absence of global states permits concurrent pattern matching. It is completed by the `ppx_regexp`, which makes using this library easier. However, only the PCRE syntax is supported.
   - **Reference:** `ppx_regexp` is described on [its page](https://github.com/paurkedal/ppx_regexp). It can be completed by the [PCRE syntax](https://www.pcre.org/original/doc/html/pcresyntax.html) or any [PCRE cheat sheet](https://www.debuggex.com/cheatsheet/regex/pcre).
