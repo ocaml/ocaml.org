3e1c3231f97ec5c08dc4a5d2facafba7ac8610f9
sabine <6594573+sabine@users.noreply.github.com>
2024-08-05 10:45:37 +0200

    minor adjustments

--- tool/ood-gen/lib/job.ml
+++ tool/ood-gen/lib/job.ml
@@ -8,10 +8,10 @@ let all () =
 module JobFeed = struct
   let create_entry (job : t) =
     let content = 
-      Syndic.Atom.Text (Printf.sprintf "Company: %s\nLocations: %s\n" job.company (String.concat ", " job.locations)) 
+      Syndic.Atom.Text (Printf.sprintf "%s is looking for a \"%s\" (%s)\n" job.company job.title (String.concat ", " job.locations))
     in
     let id = Uri.of_string (job.link) in
-    let authors = (Syndic.Atom.author "Ocaml.org", []) in
+    let authors = (Syndic.Atom.author "Job Listings on OCaml.org", []) in
     let updated = match job.publication_date with
       | Some date_str -> Syndic.Date.of_rfc3339 (date_str ^ "T00:00:00-00:00")
       | None -> Syndic.Date.of_rfc3339 "1970-01-01T00:00:00Z" in
@@ -24,7 +24,7 @@ module JobFeed = struct
     let open Rss in
     () |> all
     |> create_entries ~create_entry
-    |> entries_to_feed ~id:"job.xml" ~title:"OCaml Jobs"
+    |> entries_to_feed ~id:"jobs.xml" ~title:"OCaml Jobs"
     |> feed_to_string
 end
   
