a8d018e9e41f2827f82aac31bff2b73d09e80e8f
Cuihtlauac Alvarado <cuihtlauac@users.noreply.github.com>
2024-11-25 15:46:26 +0100

    (data) Cookbook basic concurrency recipe (#2377)
    * Minimize task.yml
    * minor line editing changes
    * editing
    
    ---------
    
    Co-authored-by: Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
    Co-authored-by: Christine Rose <christinerose@users.noreply.github.com>
    Co-authored-by: sabine <6594573+sabine@users.noreply.github.com>

---
+++ data/cookbook/create-and-await-promises/00-lwt.ml
@@ -0,0 +1,90 @@
+---
+packages:
+- name: lwt
+  tested_version: 5.7.0
+  used_libraries:
+  - lwt
+  - lwt.unix
+discussion: |
+  Lwt is a scheduler that permits concurrent execution of promises on a
+  single thread. Promise switching is performed at each long I/O (using the
+  `Lwt_io` module), timing pause (`Lwt_unix.sleep`), or explicit promise
+  switching (`Lwt.pause ()`). Promises are typically composed with the `Lwt.bind`
+  function. More details about the monad `bind` function is given in the [monad
+  section](/docs/monads). The `let` operators are detailed in the [operator
+  section](/docs/operators). Refer to [the Lwt manual](https://ocsigen.org/lwt/latest/manual/manual).
+---
+
+(* We define shorthands for the Lwt binding operators to make our concurrent code more readable.
+  `let* a = <Lwt promise> in b` schedules the Lwt promise, waits for its result,
+  then excutes `b`. In contrast, `let*? a = <promise> in b` short-circuits on error
+  by using a `Result` type: execution continues if the result is `Ok x`, and stops
+  if the result was `Error err`.
+  *)
+let ( let* ) = Lwt.bind
+let ( let*? ) = Lwt_result.bind
+
+(* Example task with return value to demonstrate concurrent execution. *)
+let task n =
+  let* () = Lwt_io.printf "Task %d - first line\n" n in
+  let* () = Lwt.pause () in
+  let* () = Lwt_io.printf "Task %d - second line\n" n in
+  Lwt.return n
+
+(* Example task that only produces side effects, without a return value.
+  *)
+let task' n =
+  let* result = task n in
+  Lwt.return_unit
+
+let () =
+(* Run two tasks in parallel and collect the results. *)
+  let run_two_parallel_tasks () = 
+    Lwt_main.run @@ Lwt.both (task 1) (task 2)
+  in
+  Printf.printf "\n= Running Two Parallel Tasks =\n";
+  let (result1, result2) = run_two_parallel_tasks () in
+  Printf.printf "Results: (%s, %s)\n" 
+    (string_of_int result1)
+    (string_of_int result2);
+
+(* Run tasks in parallel and collect results. *)
+  let parallel_map () =
+    Lwt_main.run @@ Lwt_list.map_p task [1; 2; 3]
+  in
+  Printf.printf "\n= Parallel Map Example =\n";
+  let parallel_results = parallel_map () in
+  Printf.printf "Results: [%s]\n" 
+    (String.concat "; "
+      (List.map string_of_int parallel_results));
+
+(* Run tasks sequentially and collect results. *)
+  let sequential_map () =
+    Lwt_main.run @@ Lwt_list.map_s task [1; 2; 3]
+  in
+  Printf.printf "\n= Sequential Map Example =\n";
+  let sequential_results = sequential_map () in
+  Printf.printf "Results: [%s]\n"
+    (String.concat "; "
+      (List.map string_of_int sequential_results));
+
+(* Run tasks in parallel for side effects. *)
+  let parallel_iter () =
+    Lwt_main.run @@ Lwt_list.iter_p task' [1; 2; 3]
+  in
+  Printf.printf "\n= Parallel Iteration Example =\n";
+  parallel_iter ();
+
+(* Run tasks sequentially for side effects. *)
+  let sequential_iter () =
+    Lwt_main.run @@ Lwt_list.iter_s task' [1; 2; 3]
+  in
+  Printf.printf "\n= Sequential Iteration Example =\n";
+  sequential_iter ();
+
+  let sleep_task () =
+    Lwt_main.run @@ (Lwt_unix.sleep 1.)
+  in
+  Printf.printf "\n= Sleep Example (1 second) =\n";
+  sleep_task ();
+  Printf.printf "Finished sleeping\n"
