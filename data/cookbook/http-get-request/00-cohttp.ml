74f74c455eb3d0dec9af4807c75741a71a6020b3
sabine <6594573+sabine@users.noreply.github.com>
2024-11-28 14:51:11 +0100

    editing

--- data/cookbook/http-get-request/00-cohttp.ml
+++ data/cookbook/http-get-request/00-cohttp.ml
@@ -1,48 +1,76 @@
 ---
 packages:
-- name: "lwt"
-  tested_version: "5.7.0"
-  used_libraries:
-  - lwt
-  - lwt.unix
-- name: "lwt_ppx"
-  tested_version: "2.1.0"
-  used_libraries:
-  - lwt_ppx
-- name: "cohttp"
-  tested_version: "5.3.1"
-  used_libraries:
-  - cohttp
-- name: "cohttp-lwt-unix"
-  tested_version: "5.3.0"
-  used_libraries:
-  - cohttp-lwt-unix
+  - name: "cohttp-lwt-unix"
+    tested_version: "5.3.0"
+    used_libraries:
+    - cohttp-lwt-unix
+  - name: "cohttp"
+    tested_version: "5.3.1"
+    used_libraries:
+    - cohttp
+  - name: "lwt"
+    tested_version: "5.7.0"
+    used_libraries:
+    - lwt
+    - lwt.unix
 discussion: |
-  - **About `cohttp`:** The `cohttp` provides a client and a server implementation of HTTP(S)
-  - **Reference:** The `cohttp` package is documented on the [ocaml-cohttp page](https://github.com/mirage/ocaml-cohttp).
+  The `cohttp` package provides a client and a server implementation of HTTP(S).
+
+  Note: `Lwt` is OCaml's library for handling asynchronous operations,
+    similar to Promises in JavaScript
+  The package `lwt_ppx` can be used for syntactic
+    sugar (`let%lwt` instead of having to define a custom
+    shorthand for `Lwt.bind`).
 ---
 
-let url = "https://www.ocaml.org"
+let ( let* ) = Lwt.bind
 
-(* The `get` function fetch the ressource. The status code is analysed, and if all is ok, return the body of the answer. If not, the reason of the failure *)
+(*
+This function performs an HTTP GET request to a given URL and handles the response.
+It returns a Result type: Ok with the response body if successful, or Error with an error message.
 
+Step by step:
+1. Takes a URL as a string parameter
+2. Uses `Cohttp_lwt_unix.Client.get` to make the HTTP request
+   (`let*` is used because this is an asynchronous operation
+   within the `Lwt` monad)
+3. Extracts the HTTP status code from the response
+4. If the status code indicates success (2xx):
+   - Converts the response body to a string
+   - Returns Ok with the body string
+5. If not successful:
+   - Returns Error with the HTTP error message
+*)
 let http_get url =
-  let%lwt (resp,body) = Cohttp_lwt_unix.Client.get
-                          (Uri.of_string url) in
+  let* (resp, body) =
+    Cohttp_lwt_unix.Client.get (Uri.of_string url)
+  in
   let code = resp
              |> Cohttp.Response.status
              |> Cohttp.Code.code_of_status in
   if Cohttp.Code.is_success code
   then
-    let%lwt b = Cohttp_lwt.Body.to_string body in
+    let* b = Cohttp_lwt.Body.to_string body in
     Lwt.return (Ok b)
   else
-    Lwt.return (Error (Cohttp.Code.reason_phrase_of_code code))
+    Lwt.return (Error (
+      Cohttp.Code.reason_phrase_of_code code
+    ))
 
-(* Let's call the `http_get` function. *)
+(*
+This is the main program that:
+1. Runs our `http_get` function with ocaml.org as the target
+2. Uses `Lwt_main.run` to execute our async code
+3. Handles the result:
+   - If `Error`: prints the URL and error message
+   - If `Ok`: prints the URL and the received HTML content
+4. All printing is done inside the `Lwt` monad (hence the `Lwt.return ()`)
+*)
 let () =
-  Lwt_main.run @@
-    match%lwt http_get url with
+  let url = "https://ocaml.org" in
+  Lwt_main.run (
+    let* result = http_get url in
+    match result with
     | Error str ->
        Printf.printf "%s:fail\n" url;
        Printf.printf "Error: %s\n" str;
@@ -51,3 +79,4 @@ let () =
        Printf.printf "%s:succed\n" url;
        print_string result;
        Lwt.return ()
+  )
