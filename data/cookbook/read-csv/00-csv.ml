c8cc08e2ff5093c919f90467a2e41cd3536ebd61
sabine <6594573+sabine@users.noreply.github.com>
2024-12-10 13:22:00 +0100

    editing

--- data/cookbook/read-csv/00-csv.ml
+++ data/cookbook/read-csv/00-csv.ml
@@ -1,76 +1,83 @@
 ---
-packages: 
+packages:
 - name: "csv"
   tested_version: "2.4"
   used_libraries:
   - "csv"
 ---
 
-(** This is a basic example of reading data from a CSV formatted string into a
-    list of record results. It includes some simple error handling of malformed
-    data *)
-
-(* let* allows for nicer syntax when working with result *)
+(* Cleaner syntax for Result.bind *)
 let ( let* ) = Result.bind
 
-(* Basic types which will be read from the CSV *)
-type colour = Red | Yellow | Blue
-type fruit = { index : int; name : string; colour : colour }
+(* Core types *)
+type color = Red | Yellow | Blue
 
-(* Utility function to print a colour as a string *)
-let colour_to_string = function
+type fruit = {
+  id : int;
+  name : string;
+  color : color;
+}
+
+(* String representation of colors *)
+let show_color = function
   | Red -> "Red"
   | Yellow -> "Yellow"
   | Blue -> "Blue"
 
-(* Parsing functions which take a string and return a result *)
-
-(* Match a string to a colour directly. Any other string is an
-   `Unknown_colour *)
-let parse_colour = function
+(* Parsing functions returning Results *)
+let parse_color = function
   | "Red" -> Ok Red
   | "Yellow" -> Ok Yellow
   | "Blue" -> Ok Blue
-  | _ -> Error `Unknown_colour
+  | _ -> Error `Unknown_color
 
-(* Parse the index i as an optional value and convert the option to a result *)
-let parse_index i =
-  i |> int_of_string_opt |> Option.to_result ~none:`Invalid_index
+let parse_id raw_id =
+  raw_id
+  |> int_of_string_opt
+  |> Option.to_result ~none:`Invalid_id
 
-(* Any string except "" is an acceptable fruit name *)
-let parse_name = function "" -> Error `Missing_name | f -> Ok f
+let parse_name = function
+  | "" -> Error `Missing_name
+  | n -> Ok n
 
-(** Combine let* with the above parse functions for an easy to read parse_row
-    function. An error returned at any of the let* binds will propagate upwards,
-    discontinuing the execution of parse_row. *)
+(* Parse a CSV row into a fruit record *)
 let parse_row row =
-  let* index = Csv.Row.find row "index" |> parse_index in
-  let* name = Csv.Row.find row "name" |> parse_name in
-  let* colour = Csv.Row.find row "colour" |> parse_colour in
+  let* id =
+    Csv.Row.find row "id" |> parse_id
+  in
+  let* name =
+    Csv.Row.find row "name" |> parse_name
+  in
+  let* color =
+    Csv.Row.find row "color" |> parse_color
+  in
+  Ok { id; name; color }
 
-  Ok { index; name; colour }
-
-(* map parse_row to a CSV to parse all the rows in the file *)
-let parse_fruits_csv contents =
-  contents
+(* Parse CSV string into list of fruit results *)
+let parse_fruits csv_str =
+  csv_str
   |> Csv.of_string ~has_header:true
-  |> Csv.Rows.input_all |> List.map parse_row
+  |> Csv.Rows.input_all
+  |> List.map parse_row
 
-
-let contents =
-  {|"index","name","colour",
+let sample_csv =
+  {|"id","name","color",
   "1","apple","Red",
   "2","lemon","Yellow",
   "3","berry","Blue",|}
 
-(* Example of using the above types and functions to parse a csv *)
+(* Main processing *)
 let () =
-  parse_fruits_csv contents
+  parse_fruits sample_csv
   |> List.iter (function
-       | Ok fruit ->
-           print_endline
-             (Int.to_string fruit.index ^ " " ^ fruit.name ^ " "
-             ^ colour_to_string fruit.colour)
-       | Error `Invalid_index -> print_endline "Invalid_index"
-       | Error `Unknown_colour -> print_endline "Unknown_colour"
-       | Error `Missing_name -> print_endline "Missing_name")
+    | Ok fruit ->
+        Printf.printf "%d %s %s\n"
+          fruit.id
+          fruit.name
+          (show_color fruit.color)
+    | Error `Invalid_id ->
+        print_endline "Invalid_id"
+    | Error `Unknown_color ->
+        print_endline "Unknown_color"
+    | Error `Missing_name ->
+        print_endline "Missing_name")
