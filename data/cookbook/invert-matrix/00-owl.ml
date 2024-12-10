1c15019814148e00141feebb67ea80c9712a5de9
sabine <6594573+sabine@users.noreply.github.com>
2024-12-10 14:56:58 +0100

    minor editing

--- data/cookbook/invert-matrix/00-owl.ml
+++ data/cookbook/invert-matrix/00-owl.ml
@@ -5,12 +5,20 @@ packages:
   used_libraries:
   - owl
 ---
+
 let m = Owl.Mat.of_array [|1.;2.;3.;4.|] 2 2
 let minv = Owl.Linalg.D.inv m
 
 let () =
-Owl.Mat.iteri_2d (fun i j a -> Printf.printf "(%i,%i) %.1f\n" i j a) minv;;
+  Owl.Mat.iteri_2d
+    (fun i j a ->
+      Printf.printf "(%i,%i) %.1f\n" i j a)
+    minv;;
 
-(* A verification *)
-
-let () = assert Owl.Mat.( m *@ minv =~ eye 2)
+(*
+Verify that the inverse is correct by checking if
+m * m^(-1) = Identity matrix
+=~ operator checks if matrices are approximately equal
+(within numerical precision)
+*)
+let () = assert Owl.Mat.(m *@ minv =~ eye 2)
--- data/cookbook/normalize-vector/00-owl.ml
+++ data/cookbook/normalize-vector/00-owl.ml
@@ -5,12 +5,23 @@ packages:
   used_libraries:
   - owl
 ---
+
+(*
+Create a 2x1 matrix (column vector) with values
+[1.; 1.]
+*)
 let v = Owl.Mat.of_array [|1.;1.|] 2 1
 
+(* Calculate the Euclidean norm of the vector *)
 let norm = Owl.Linalg.D.norm v
 
+(*
+The /$ operator performs element-wise division
+between a matrix and a scalar
+*)
 let v' = Owl.Mat.( v /$ norm );;
 
 let () =
-  Owl.Mat.iteri_2d (fun i j a -> Printf.printf "(%i) %.1f\n" i a) v';;
-
+  Owl.Mat.iteri_2d
+    (fun i j a -> Printf.printf "(%i) %.1f\n" i a)
+    v';;
