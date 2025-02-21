48e8d113c74817c0fe1256676c0b280d6d7b0781
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2025-02-21 12:04:22 +0100

    Add deriver for Video, part 1

--- tool/ood-gen/lib/video.ml
+++ tool/ood-gen/lib/video.ml
@@ -1,7 +1,3 @@
-open Data_intf.Video
-
-type video_list = t list [@@deriving show]
-
 let all () = Youtube.all () @ Watch.all ()
 
 let template () =
@@ -9,7 +5,7 @@ let template () =
 include Data_intf.Video
 let all =%a
 |ocaml}
-    pp_video_list (all ())
+    Vid.pp_video_list (all ())
 
 let scrape () =
   Youtube.scrape "data/video-youtube.yml";
