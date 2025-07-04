bed169ba9468005f2d1e018ab9679bc33d51e924
Cuihtlauac Alvarado <cuihtlauac@users.noreply.github.com>
2025-07-04 11:39:27 +0200

    fix 3194 (#3197)
    * Fix syntax error
    
    * Remove trailing whitespace

--- data/cookbook/deserialise-from-json/00-ppx_yojson_conv.ml
+++ data/cookbook/deserialise-from-json/00-ppx_yojson_conv.ml
@@ -19,22 +19,22 @@ let json = {|
 }
 |}
 
-(* The annotation `[@@deriving of_yojson]` causes the PPX from `ppx_yojson_conv` to generate a function
+(* The annotation `[@@deriving yojson]` causes the PPX from `ppx_yojson_conv` to generate a function
   `language_of_yojson` that converts values from type `Yojson.Safe.t` to `language`. *)
-type language = { 
-  name: string; 
-  url: string 
-} [@@deriving of_yojson]
+type language = {
+  name: string;
+  url: string
+} [@@deriving yojson]
 
 (* First we convert the JSON string to `Yojson.Safe.t` and then we use the generated function to create the record.
    If parsing fails, an `Of_yojson_error` exception is thrown that we can handle. *)
 try
-  let result = 
+  let result =
     json
-    |> Yojson.Safe.from_string 
-    |> language_of_yojson 
+    |> Yojson.Safe.from_string
+    |> language_of_yojson in
   Some result
-with Ppx_yojson_conv_lib.Yojson_conv.Of_yojson_error (exception, _) ->
-  let _ = 
-    Printexc.to_string exception print_endline in
+with Ppx_yojson_conv_lib.Yojson_conv.Of_yojson_error (exc, _) ->
+  let _ =
+    print_endline (Printexc.to_string exc) in
   None
