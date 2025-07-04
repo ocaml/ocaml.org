203b72da1065cc23760b3f78ee2466981ec5cf3b
Cuihtlauac ALVARADO <cuihtlauac@tarides.com>
2025-07-04 08:47:00 +0200

    Fix syntax error

--- data/cookbook/deserialise-from-json/00-ppx_yojson_conv.ml
+++ data/cookbook/deserialise-from-json/00-ppx_yojson_conv.ml
@@ -19,12 +19,12 @@ let json = {|
 }
 |}
 
-(* The annotation `[@@deriving of_yojson]` causes the PPX from `ppx_yojson_conv` to generate a function
+(* The annotation `[@@deriving yojson]` causes the PPX from `ppx_yojson_conv` to generate a function
   `language_of_yojson` that converts values from type `Yojson.Safe.t` to `language`. *)
 type language = { 
   name: string; 
   url: string 
-} [@@deriving of_yojson]
+} [@@deriving yojson]
 
 (* First we convert the JSON string to `Yojson.Safe.t` and then we use the generated function to create the record.
    If parsing fails, an `Of_yojson_error` exception is thrown that we can handle. *)
@@ -32,9 +32,9 @@ try
   let result = 
     json
     |> Yojson.Safe.from_string 
-    |> language_of_yojson 
+    |> language_of_yojson in
   Some result
-with Ppx_yojson_conv_lib.Yojson_conv.Of_yojson_error (exception, _) ->
+with Ppx_yojson_conv_lib.Yojson_conv.Of_yojson_error (exc, _) ->
   let _ = 
-    Printexc.to_string exception print_endline in
+    print_endline (Printexc.to_string exc) in
   None
