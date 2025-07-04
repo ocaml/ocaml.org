d75e26bd82583d5387bc11b501490ca19af89723
Cuihtlauac ALVARADO <cuihtlauac@tarides.com>
2025-07-04 08:47:27 +0200

    Remove trailing whitespace

--- data/cookbook/deserialise-from-json/00-ppx_yojson_conv.ml
+++ data/cookbook/deserialise-from-json/00-ppx_yojson_conv.ml
@@ -21,20 +21,20 @@ let json = {|
 
 (* The annotation `[@@deriving yojson]` causes the PPX from `ppx_yojson_conv` to generate a function
   `language_of_yojson` that converts values from type `Yojson.Safe.t` to `language`. *)
-type language = { 
-  name: string; 
-  url: string 
+type language = {
+  name: string;
+  url: string
 } [@@deriving yojson]
 
 (* First we convert the JSON string to `Yojson.Safe.t` and then we use the generated function to create the record.
    If parsing fails, an `Of_yojson_error` exception is thrown that we can handle. *)
 try
-  let result = 
+  let result =
     json
-    |> Yojson.Safe.from_string 
+    |> Yojson.Safe.from_string
     |> language_of_yojson in
   Some result
 with Ppx_yojson_conv_lib.Yojson_conv.Of_yojson_error (exc, _) ->
-  let _ = 
+  let _ =
     print_endline (Printexc.to_string exc) in
   None
