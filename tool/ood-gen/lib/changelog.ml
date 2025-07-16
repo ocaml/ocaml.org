1e57ff16523808f61744a254c5bda808636bccf0
sabine <6594573+sabine@users.noreply.github.com>
2025-07-16 15:37:31 +0200

    update scraper to add releases

--- tool/ood-gen/lib/changelog.ml
+++ tool/ood-gen/lib/changelog.ml
@@ -112,10 +112,16 @@ module Releases = struct
     versions : string list option;
   }
   [@@deriving
-    of_yaml,
+    yaml,
       stable_record ~version:release ~remove:[ changelog; description ]
         ~modify:[ authors; contributors; versions ]
         ~add:[ slug; changelog_html; body_html; body; date; project_name ]]
+
+  let pp_meta ppf v =
+    Fmt.pf ppf {|---
+%s---
+|}
+      (release_metadata_to_yaml v |> Yaml.to_string |> Result.get_ok)
 
   let of_release_metadata m =
     release_metadata_to_release m ~modify_authors:(Option.value ~default:[])
@@ -286,7 +292,7 @@ module Scraper = struct
   let fetch_github repo =
     [ River.fetch { River.name = repo; url = repo ^ "/releases.atom" } ]
     |> River.posts
-    |> List.map (fun post -> River.title post)
+    |> List.map (fun post -> (River.title post, post))
 
   let group_releases_by_project all =
     List.fold_left
@@ -296,17 +302,48 @@ module Scraper = struct
           acc t.versions)
       SMap.empty all
 
+  let write_release_announcement_file project version (post : River.post) =
+    let yyyy_mm_dd =
+      River.date post |> Option.get |> Ptime.to_rfc3339
+      |> String.split_on_char 'T' |> List.hd
+    in
+    let title = River.title post in
+    let output_file =
+      "data/changelog/releases/" ^ project ^ "/" ^ yyyy_mm_dd ^ "-" ^ project
+      ^ "-" ^ version ^ ".md"
+    in
+    let content = River.content post in
+    let description = River.meta_description post in
+    let author = River.author post in
+    let metadata : Releases.release_metadata =
+      {
+        title;
+        tags = [ project ];
+        contributors = None;
+        description;
+        changelog = None;
+        versions = None;
+        authors = Some [ author ];
+      }
+    in
+    let s = Format.asprintf "%a\n%s\n" Releases.pp_meta metadata content in
+    let oc = open_out output_file in
+    Printf.fprintf oc "%s" s;
+    close_out oc
+
   let check_if_uptodate project known_versions =
     let known_versions = SSet.of_list known_versions in
-    let check scraped_versions =
+    let check repo =
+      let scraped_versions = fetch_github repo in
       List.iter
-        (fun v ->
-          if not (SSet.mem v known_versions) then
-            warn "No changelog entry for %S version %S\n%!" project v)
+        (fun (version, post) ->
+          if not (SSet.mem version known_versions) then (
+            warn "No changelog entry for %S version %S\n%!" project version;
+            write_release_announcement_file project version post))
         scraped_versions
     in
     match List.assoc_opt project projects_release_feeds with
-    | Some (`Github repo) -> check (fetch_github repo)
+    | Some (`Github repo) -> check repo
     | None ->
         warn
           "Don't know how to lookup project %S. Please update \
@@ -314,7 +351,10 @@ module Scraper = struct
            %!"
           project
 
+<<<<<<< HEAD
   (** This does not generate any file. *)
+=======
+>>>>>>> 092794a6 (update scraper to add releases)
   let scrape_platform_releases () =
     Releases.all () |> group_releases_by_project |> SMap.iter check_if_uptodate
 end
